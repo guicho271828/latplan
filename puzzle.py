29e336dfa7d3420823e2291b21a5a0a9ef7d8063
Masataro Asai <guicho2.71828@gmail.com>
2017-01-02 14:07:24 +0900

    puzzle transitions

--- puzzle.py
+++ puzzle.py
@@ -124,7 +124,6 @@ def successors(config,width,height):
     pos = config[0]
     x = pos % width
     y = pos // width
-    print x,y
     succ = []
     if x is not 0:
         c = list(config)
@@ -151,7 +150,16 @@ def successors(config,width,height):
         c[other] -= width
         succ.append(c)
     return succ
-    
+
+def config_transitions(configs):
+    return [ (c1,c2) for c2 in successors(c1) for c1 in configs ]
+
+def puzzle_transitions(width, height):
+    digit = width * height
+    configs = generate_configs(digit)
+    transitions = np.array([ generate_puzzle([c1,c2],width,height)
+                             for c1 in configs for c2 in successors(c1,width,height) ])
+    return np.einsum('ab...->ba...',transitions)
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -159,7 +167,28 @@ if __name__ == '__main__':
         plt.figure(figsize=(6,6))
         plt.imshow(a,interpolation='nearest',cmap='gray',)
         plt.savefig(name)
+    def plot_grid(images,name="plan.png"):
+        import matplotlib.pyplot as plt
+        l = len(images)
+        w = 6
+        h = max(l//6,1)
+        plt.figure(figsize=(20, h*2))
+        for i,image in enumerate(images):
+            # display original
+            ax = plt.subplot(h,w,i+1)
+            plt.imshow(image,interpolation='nearest',cmap='gray',)
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+        plt.savefig(name)
     configs = generate_configs(6)
-    plot_image(generate_puzzle(configs, 2, 3)[10],"samples/puzzle.png")
-
-    
+    puzzles = generate_puzzle(configs, 2, 3)
+    print puzzles[10]
+    plot_image(puzzles[10],"samples/puzzle.png")
+    plot_grid(puzzles[:36],"samples/puzzles.png")
+    transitions = puzzle_transitions(2,3)
+    print transitions.shape
+    transitions_for_show = \
+        np.einsum('ba...->ab...',transitions) \
+          .reshape((-1,)+transitions.shape[2:])
+    print transitions_for_show.shape
+    plot_grid(transitions_for_show[:36],"samples/puzzle_transitions.png")
