b1f444013c927e11fce293bb99deee078039c4a6
Masataro Asai <guicho2.71828@gmail.com>
2017-01-03 12:57:14 +0900

    mnist_puzzle_trainsitions

--- mnist_puzzle.py
+++ mnist_puzzle.py
@@ -1,13 +1,14 @@
 import numpy as np
 from puzzle import generate_configs
 
+from mnist import mnist
+x_train, y_train, _, _ = mnist()
+filters = [ np.equal(i,y_train) for i in range(10) ]
+imgs    = [ x_train[f] for f in filters ]
+panels  = [ imgs[0].reshape((28,28)) for imgs in imgs ]
+
 def generate_mnist_puzzle(configs, width, height):
     assert width*height <= 9
-    from mnist import mnist
-    x_train, y_train, _, _ = mnist()
-    filters = [ np.equal(i,y_train) for i in range(10) ]
-    imgs    = [ x_train[f] for f in filters ]
-    panels  = [ imgs[0].reshape((28,28)) for imgs in imgs ]
     base_width = 28
     base_height = 28
     dim_x = base_width*width
@@ -22,13 +23,46 @@ def generate_mnist_puzzle(configs, width, height):
         return figure
     return np.array([ generate(c) for c in configs ]).reshape((-1,dim_y,dim_x))
 
+def mnist_puzzle_transitions(width, height):
+    from puzzle import successors
+    digit = width * height
+    configs = generate_configs(digit)
+    transitions = np.array([ generate_mnist_puzzle([c1,c2],width,height)
+                             for c1 in configs for c2 in successors(c1,width,height) ])
+    return np.einsum('ab...->ba...',transitions)
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     def plot_image(a,name):
         plt.figure(figsize=(6,6))
-        plt.imshow(a)
+        plt.imshow(a,interpolation='nearest',cmap='gray',)
+        plt.savefig(name)
+    def plot_grid(images,name="plan.png"):
+        import matplotlib.pyplot as plt
+        l = len(images)
+        w = 6
+        h = max(l//6,1)
+        plt.figure(figsize=(20, h*2))
+        for i,image in enumerate(images):
+            # display original
+            ax = plt.subplot(h,w,i+1)
+            plt.imshow(image,interpolation='nearest',cmap='gray',)
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
         plt.savefig(name)
     configs = generate_configs(6)
-    plot_image(generate_mnist_puzzle(configs, 2, 3)[10],"samples/mnist_puzzle.png")
+    puzzles = generate_mnist_puzzle(configs, 2, 3)
+    print puzzles[10]
+    plot_image(puzzles[10],"samples/mnist_puzzle.png")
+    plot_grid(puzzles[:36],"samples/mnist_puzzles.png")
+    transitions = mnist_puzzle_transitions(2,3)
+    import numpy.random as random
+    indices = random.randint(0,transitions[0].shape[0],18)
+    transitions = transitions[:,indices]
+    print transitions.shape
+    transitions_for_show = \
+        np.einsum('ba...->ab...',transitions) \
+          .reshape((-1,)+transitions.shape[2:])
+    print transitions_for_show.shape
+    plot_grid(transitions_for_show,"samples/mnist_puzzle_transitions.png")
 
-    
