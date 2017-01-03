2392808f1a2e52f1d806d7a7e0706f0052057929
Masataro Asai <guicho2.71828@gmail.com>
2017-01-03 13:21:50 +0900

    test mnist_puzzle_transitions + strips.py

--- strips.py
+++ strips.py
@@ -12,7 +12,10 @@ def reshape_batch1D(images,repeat=None):
 def dump_actions(transitions,path):
     # assert 2 == transitions.shape[0]
     ae = GumbelAE(path)
-    ae.train(np.concatenate(transitions,axis=0))
+    ae.train(np.concatenate(transitions,axis=0),
+             # anneal_rate=0.0001,
+             # epoch=400
+    )
 
     orig, dest = transitions[0], transitions[1]
     orig_b, dest_b = ae.encode_binary(orig), ae.encode_binary(dest)
@@ -23,6 +26,7 @@ def dump_actions(transitions,path):
 
 
 if __name__ == '__main__':
+    import numpy.random as random
     def plot_grid(images,name="plan.png"):
         import matplotlib.pyplot as plt
         l = len(images)
@@ -37,33 +41,33 @@ if __name__ == '__main__':
             ax.get_yaxis().set_visible(False)
         plt.savefig(name)
 
-    # from counter import counter_transitions
-    # transitions = counter_transitions(n=1000)
-    # counter_actions = dump_actions((reshape_batch1D(transitions[0]),
-    #                                 reshape_batch1D(transitions[1])),
-    #                                "samples/counter_model/")
-    # print counter_actions[:3]
-    # ae = GumbelAE("samples/counter_model/")
-    # xs = transitions[0][:18]
-    # ys = ae.autoencode(xs)
-    # images = np.reshape(np.einsum('ab...->ba...',(xs,ys)),(36,28,28))
-    # plot_grid(images, ae.local("autoencoding.png"))
-    
+    def run(path, shape, transitions):
+        actions = dump_actions(transitions, path)
+        print actions[:3]
+        ae = GumbelAE(path)
+        xs = transitions[0][random.randint(0,transitions[0].shape[0],12)]
+        zs = ae.encode_binary(xs).reshape((-1,4,4))
+        ys = ae.autoencode(xs).reshape(shape)
+        xs = xs.reshape(shape)
+        images = []
+        for x,z,y in zip(xs,zs,ys):
+            images.append(x)
+            images.append(z)
+            images.append(y)
+        plot_grid(images, ae.local("autoencoding.png"))
+
+    from counter import counter_transitions
+    transitions = counter_transitions(n=1000)
+    run("samples/counter_model/", (-1,28,28), transitions)
     
     from puzzle import puzzle_transitions
     transitions = puzzle_transitions(2,2)
-    puzzle_actions = dump_actions((reshape_batch1D(transitions[0],1000),
-                                   reshape_batch1D(transitions[1],1000)),
-                                  "samples/puzzle_model/")
-    print puzzle_actions[:3]
-    ae = GumbelAE("samples/puzzle_model/")
-    import numpy.random as random
-    xs = transitions[0][random.randint(0,transitions[0].shape[0],12)]
-    zs = ae.encode_binary(xs).reshape((-1,4,4))
-    ys = ae.autoencode(xs).reshape((-1,6*2,5*2))
-    images = []
-    for x,z,y in zip(xs,zs,ys):
-        images.append(x)
-        images.append(z)
-        images.append(y)
-    plot_grid(images, ae.local("autoencoding.png"))
+    transitions = np.repeat(transitions,100,axis=1)
+    run("samples/puzzle_model/", (-1,6*2,5*2), transitions)
+
+    from mnist_puzzle import mnist_puzzle_transitions
+    transitions = mnist_puzzle_transitions(2,2)
+    transitions = np.repeat(transitions,100,axis=1)
+    run("samples/mnist_puzzle_model/", (-1,28*2,28*2), transitions)
+
+    
