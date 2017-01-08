41348f1c9f7cf4b062aecc0c5b13dde08915c1c2
Masataro Asai <guicho2.71828@gmail.com>
2017-01-09 01:27:45 +0900

    reorder visualize

--- visualize.py
+++ visualize.py
@@ -2,41 +2,28 @@
 
 import numpy as np
 from model import GumbelAE, ConvolutionalGumbelAE
-# from quiver_engine import server
-
-# ae = GumbelAE("samples/puzzle22_model/")
-ae = ConvolutionalGumbelAE("samples/puzzle22_modelc/")
-ae.load()
-# server.launch(ae.encoder)
-
 from keras.layers import Input, Dense, Dropout, Convolution2D, MaxPooling2D, UpSampling2D, Reshape, Flatten, Activation, Cropping2D, SpatialDropout2D, Lambda, GaussianNoise
 from keras.models import Model, Sequential
-
-conv_layers = [ l for l in ae.encoder.layers if isinstance(l,Convolution2D) ]
+# from quiver_engine import server
+# server.launch(ae.encoder)
+import puzzle
+import numpy.random as random
 
 from plot import plot_grid, plot_grid2
 
-def print_conv():
+def print_conv(ae):
+    conv_layers = [ l for l in ae.encoder.layers if isinstance(l,Convolution2D) ]
     for i,conv in enumerate(conv_layers):
         W = conv.get_weights()[0]
         path = ae.local("conv_filters{}.png".format(i))
         print path
         plot_grid(np.einsum("xycf->fxy",W),path=path)
 
-print_conv()
-
-import puzzle
-xs = puzzle.states(2,2)
-import numpy.random as random
-xs = xs[random.randint(0,xs.shape[0],12)]
-
-_x = ae.encoder.layers[0]
-
-def print_activation():
+def print_activation(xs,shape):
     path = ae.local("activation{}.png".format(0))
     print path, xs.shape
     plot_grid(xs,path=path)
-    x = Input(_x.input_shape[1:])
+    x = Input(shape)
     now = x
     for i,l in enumerate(ae.encoder.layers[1:]):
         now = l(now)
@@ -47,4 +34,12 @@ def print_activation():
             print path
             plot_grid(now_images,path=path)
 
-print_activation()
+# ae = GumbelAE("samples/puzzle32_model/")
+ae = ConvolutionalGumbelAE("samples/puzzle32p_modelc/")
+ae.load()
+
+print_conv(ae)
+
+xs = puzzle.states(3,2)
+xs = xs[random.randint(0,xs.shape[0],12)]
+print_activation(xs,ae.encoder.layers[0].input_shape[1:])
