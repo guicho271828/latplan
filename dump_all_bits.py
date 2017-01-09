4df9b178e6d0b2a95d65c349dfe45cecf31da20d
Masataro Asai <guicho2.71828@gmail.com>
2017-01-09 12:11:10 +0900

    fix dump_all_bits

--- dump_all_bits.py
+++ dump_all_bits.py
@@ -1,48 +1,13 @@
 #!/usr/bin/env python
 
 import numpy as np
-from model import GumbelAE
+from model import GumbelAE, ConvolutionalGumbelAE
+from plot import plot_grid, plot_grid2
 
-# if __name__ == '__main__':
-
-def plot_grid(images,name="plan.png"):
-    import matplotlib.pyplot as plt
-    l = len(images)
-    w = 32
-    h = l//w+1
-    plt.figure(figsize=(60, h*2))
-    for i,image in enumerate(images):
-        # display original
-        ax = plt.subplot(h,w,i+1)
-        plt.imshow(image,interpolation='nearest',cmap='gray',)
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    plt.savefig(name)
-
-def plot_grid2(images,shape,w,name="plan.png"):
-    import matplotlib.pyplot as plt
-    l = images.shape[0]
-    h = l//w
-    margin = 3
-    m_shape = (margin + np.array(shape))
-    figure = np.ones(m_shape * np.array((h,w)))
-    # print images.shape,h,w
-    for y in range(h):
-        for x in range(w):
-            begin = m_shape * np.array((y,x))
-            end   = (m_shape * (np.array((y,x))+1)) - margin
-            # print begin,end,y*w+x
-            figure[begin[0]:end[0],begin[1]:end[1]] = images[y*w+x]
-    plt.figure(figsize=(h,w))
-    plt.imshow(figure,interpolation='nearest',cmap='gray',)
-    plt.savefig(name)
-
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-
-ae = GumbelAE("samples/puzzle3p_model/")
-ae.build((12,15))
+ae = GumbelAE("samples/puzzle32p_model/")
+# ae = ConvolutionalGumbelAE("samples/puzzle32p_modelc/")
 ae.load()
+
 m = 16
 
 zs = (((np.arange(2**m)[:,None] & (1 << np.arange(m)))) > 0).astype(int)
@@ -61,6 +26,6 @@ def run():
         print path
         # plot_grid(images,path)
         # plot_grid(ys[j*per_image:(1+j)*per_image],path)
-        plot_grid2(ys[j*per_image:(1+j)*per_image],(12,15),16,path)
+        plot_grid2(ys[j*per_image:(1+j)*per_image],shape=(12,15),w=16,path=path)
 
 run()
