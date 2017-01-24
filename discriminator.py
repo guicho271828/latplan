3f68564b61bdda28c75376bc35384093ef8ec3d9
Masataro Asai <guicho2.71828@gmail.com>
2017-01-24 14:11:36 +0900

    fix!

--- discriminator.py
+++ discriminator.py
@@ -43,8 +43,8 @@ if __name__ == '__main__':
 
     d1 = np.concatenate((train, train_random), axis=0)
     d2 = np.concatenate((test,  test_random),  axis=0)
-    l1 = np.concatenate((np.ones(len(train)), np.zeros(len(train_random))), axis=0)
-    l2 = np.concatenate((np.ones(len(test)), np.zeros(len(test_random))), axis=0)
+    l1 = np.concatenate((one_hot_ones(len(train)), one_hot_zeros(len(train_random))), axis=0)
+    l2 = np.concatenate((one_hot_ones(len(test)), one_hot_zeros(len(test_random))), axis=0)
     # raw_discriminator = Discriminator("samples/mnist_puzzle33p_raw_discriminator/",
                                       # {'layer':2000,'dropout':0.4})
     # raw_discriminator.train(d1, test_data=d2, train_data_to=l1, test_data_to=l2)
