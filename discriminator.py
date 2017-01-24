bf098f4f53efd9e12eb45d487024e989b9b8427c
Masataro Asai <guicho2.71828@gmail.com>
2017-01-24 14:06:44 +0900

    bugfix: name all parameters and misc

--- discriminator.py
+++ discriminator.py
@@ -18,6 +18,15 @@ from plot import plot_ae
 def select(data,num):
     return data[random.randint(0,data.shape[0],num)]
 
+def one_hot_ones(l):
+    return np.concatenate(
+        (np.ones([l,1]),np.zeros([l,1])),
+        axis=-1)
+def one_hot_zeros(l):
+    return np.concatenate(
+        (np.zeros([l,1]),np.ones([l,1])),
+        axis=-1)
+
 if __name__ == '__main__':
     import numpy.random as random
     from trace import trace
@@ -35,14 +44,16 @@ if __name__ == '__main__':
     d1 = np.concatenate((train, train_random), axis=0)
     d2 = np.concatenate((test,  test_random),  axis=0)
     l1 = np.concatenate((np.ones(len(train)), np.zeros(len(train_random))), axis=0)
-    l2 = np.concatenate((np.ones(len(test)),  np.zeros(len(test_random))), axis=0)
-    raw_discriminator = Discriminator("samples/mnist_puzzle33p_raw_discriminator/",{0:2000,1:0.4}) 
-    raw_discriminator.train(d1, test_data=d2, train_data_to=l1, test_data_to=l2)
+    l2 = np.concatenate((np.ones(len(test)), np.zeros(len(test_random))), axis=0)
+    # raw_discriminator = Discriminator("samples/mnist_puzzle33p_raw_discriminator/",
+                                      # {'layer':2000,'dropout':0.4})
+    # raw_discriminator.train(d1, test_data=d2, train_data_to=l1, test_data_to=l2)
 
     ae = GumbelAE("samples/mnist_puzzle33p_model/").load()
     b1 = ae.encode_binary(d1)
     b2 = ae.encode_binary(d2)
-    bin_discriminator = Discriminator("samples/mnist_puzzle33p_bin_discriminator/",{0:2000,1:0.4})
+    bin_discriminator = Discriminator("samples/mnist_puzzle33p_bin_discriminator/",
+                                      {'layer':2000,'dropout':0.4})
     bin_discriminator.train(b1, test_data=b2, train_data_to=l1, test_data_to=l2)
     
     
