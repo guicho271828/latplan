58546d339ec993edd3e103ce522fe13cf2d030dd
Masataro Asai <guicho2.71828@gmail.com>
2017-02-04 23:28:43 +0900

    mandrill

--- strips.py
+++ strips.py
@@ -246,6 +246,20 @@ def run_lenna_puzzle():
     dump(ae, train,test)
     dump_all_actions(ae,configs,lambda configs: p.transitions(3,3,configs))
 
+def run_mandrill_puzzle():
+    import puzzles.mandrill_puzzle as p
+    configs = p.generate_configs(9)
+    configs = np.array([ c for c in configs ])
+    random.shuffle(configs)
+    train_c = configs[:12000]
+    test_c  = configs[12000:13000]
+    train       = p.states(3,3,train_c)
+    test        = p.states(3,3,test_c)
+    print(len(configs),len(train),len(test))
+    ae = run(True,"samples/mandrill_puzzle33p_model/", train, test)
+    dump(ae, train,test)
+    dump_all_actions(ae,configs,lambda configs: p.transitions(3,3,configs))
+
 def run_spider_puzzle():
     import puzzles.spider_puzzle as p
     configs = p.generate_configs(9)
