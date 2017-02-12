3324a5a243f23420f3d1ae5b71d1e5c55b275d2b
Masataro Asai <guicho2.71828@gmail.com>
2017-02-12 16:08:44 +0900

    run_all tmp

--- run_all.sh
+++ run_all.sh
@@ -2,13 +2,23 @@
 
 trap exit SIGINT
 
+# gauss fc fc2 fcg fcg2 conv convg 
+# for type in gauss ; do
+#     for task in digital_lightsout ; do
+#         for mode in learn ; do
+#             ./strips.py $type $task $mode
+#             samples/sync.sh
+#         done
+#     done
+# done
+
 # hanoi: finished.
-# lightsout: finished.
-# mnist, mandrill: 
-#  mnist_puzzle mandrill_puzzle
-# digital_lightsout 
-for type in fc ; do
-    for task in hanoi  ; do
+# digital_lightsout: finished.
+# mnist_puzzle mandrill_puzzle: finished.
+# random_mnist_puzzle: not finished yet.
+
+for type in fc2 ; do
+    for task in hanoi mnist_puzzle mandrill_puzzle lenna_puzzle ; do
         for mode in learn ; do
             ./strips.py $type $task $mode
             samples/sync.sh
@@ -16,9 +26,11 @@ for type in fc ; do
     done
 done
 
-# for type in fc fcg ; do
+#  fcg2 1000
+#  convg2 convg conv2 conv fc2 fc
+# for type in convg ; do
 #     for task in random_mnist_puzzle ; do
-#         for mode in dump ; do
+#         for mode in learn ; do
 #             ./strips.py $type $task $mode
 #             samples/sync.sh
 #         done
