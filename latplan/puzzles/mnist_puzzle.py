b129cb866e61619b62a253047a498614b62fb401
Masataro Asai <guicho2.71828@gmail.com>
2017-02-15 12:05:59 +0900

    fixup! test-mnist: pick the "good" mnist

--- latplan/puzzles/mnist_puzzle.py
+++ latplan/puzzles/mnist_puzzle.py
@@ -7,11 +7,13 @@ from .mnist import mnist
 x_train, y_train, _, _ = mnist()
 filters = [ np.equal(i,y_train) for i in range(10) ]
 imgs    = [ x_train[f] for f in filters ]
-panels  = np.array([ imgs[0].reshape((28,28)) for imgs in imgs ])
+panels  = [ imgs[0].reshape((28,28)) for imgs in imgs ]
 
 panels[8] = imgs[8][3].reshape((28,28))
 panels[1] = imgs[1][3].reshape((28,28))
 panels.append(np.random.uniform(0,1,(28,28)))
+
+panels = np.array(panels)
 
 base = 14
 stepy = panels[0].shape[0]//base
