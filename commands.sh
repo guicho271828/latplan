73bab40bd33e1fdf07c8d8406eaed2b0f65cf97b
Masataro Asai <guicho2.71828@gmail.com>
2017-02-18 10:52:37 +0900

    cleaned commands

--- commands.sh
+++ commands.sh
@@ -2,38 +2,8 @@
 
 ./plan.py blind 'run_hanoi4    ( "samples/hanoi4_fc2"                  ,"fc2", import_module("puzzles.hanoi"                    ) )' |& tee blind-hanoi4
 ./plan.py blind 'run_hanoi10   ( "samples/hanoi10_fc2"                 ,"fc2", import_module("puzzles.hanoi"                    ) )' |& tee blind-hanoi10
-./plan.py blind 'run_puzzle    ( "samples/mnist_puzzle33p_fc2"         ,"fc2", import_module("puzzles.mnist_puzzle"             ) )' |& tee blind-mnist     
-./plan.py blind 'run_puzzle    ( "samples/mandrill_puzzle33p_fc2"      ,"fc2", import_module("puzzles.mandrill_puzzle"          ) )' |& tee blind-mandrill 
 ./plan.py blind 'run_lightsout ( "samples/digital_lightsout_fc2"       ,"fc2", import_module("puzzles.digital_lightsout"        ) )' |& tee blind-lightsout
-./plan.py blind 'run_lightsout ( "samples/digital_lightsout_skewed_fc" ,"fc",  import_module("puzzles.digital_lightsout_skewed" ) )' |& tee blind-lightsout-skewed
 ./plan.py blind 'run_lightsout3 ( "samples/digital_lightsout_skewed3_fc" ,"fc",  import_module("puzzles.digital_lightsout_skewed" ) )' |& tee blind-lightsout-skewed3
+./plan.py blind 'run_puzzle    ( "samples/mandrill_puzzle33p_fc2"      ,"fc2", import_module("puzzles.mandrill_puzzle"          ) )' |& tee blind-mandrill 
+./plan.py blind 'run_puzzle    ( "samples/mnist_puzzle33p_fc2"         ,"fc2", import_module("puzzles.mnist_puzzle"             ) )' |& tee blind-mnist     
 
-################################################################
-
-for h in blind pdb mands ; do
-    ./plan.py $h 'run_puzzle    ( "samples/mandrill_puzzle33p_fc2"      ,"fc2", import_module("puzzles.mandrill_puzzle"          ) )' |& tee $h-mandrill
-done
-
-for h in blind pdb mands ; do
-    ./plan_noise.py $h 'run_puzzle    ( "samples/mandrill_puzzle33p_fc2"      ,"fc2", import_module("puzzles.mandrill_puzzle"          ) )' |& tee $h-mandrill-noise
-done
-
-
-for h in blind pdb mands ; do
-    ./plan.py $h 'run_puzzle    ( "samples/mnist_puzzle33p_fc2"         ,"fc2", import_module("puzzles.mnist_puzzle"             ) )' |& tee $h-mnist  
-done
-
-
-for h in blind pdb mands ; do
-    ./plan_noise.py $h 'run_puzzle    ( "samples/mnist_puzzle33p_fc2"         ,"fc2", import_module("puzzles.mnist_puzzle"             ) )' |& tee $h-mnist-noise
-done
-
-
-
-
-./plan_iter.py blind 'run_puzzle    ( "samples/mnist_puzzle33p_fc2"         ,"fc2", import_module("puzzles.mnist_puzzle"             ) )' |& tee blind-mnist-iter
-
-
-
-
-./plan.py blind 'run_lightsout3 ( "samples/digital_lightsout_skewed3_fc" ,"fc",  import_module("puzzles.digital_lightsout_skewed" ) )' |& tee blind-lightsout-skewed3
