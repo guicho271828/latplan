5d9a4ea81398909de66b029951a062e55153356c
Masataro Asai <guicho2.71828@gmail.com>
2017-02-22 23:48:45 +0900

    removed unused hanoi

--- more-hanoi/blind-hanoi6.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./plan.py blind 'run_hanoi("samples/hanoi6_fc2/","fc2",import_module("puzzles.hanoi"),6)' |& tee $(dirname $0)/blind-hanoi6.log
--- more-hanoi/blind-hanoi7.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./plan.py blind 'run_hanoi("samples/hanoi7_fc2/","fc2",import_module("puzzles.hanoi"),7)' |& tee $(dirname $0)/blind-hanoi7.log
