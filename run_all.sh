b2a0952b9ed8e9737f6bc36fc1eb3f3fd9adb0d0
Masataro Asai <guicho2.71828@gmail.com>
2017-04-29 14:47:30 +0900

    [cosmetic change] run_all.sh more understandable

--- run_all.sh
+++ run_all.sh
@@ -2,9 +2,9 @@
 
 trap exit SIGINT
 
-./strips.py fc2 mnist_puzzle learn_dump 3
+./strips.py fc2 mnist_puzzle learn_dump 3 3
 samples/sync.sh
-./strips.py fc2 mandrill_puzzle learn_dump 3
+./strips.py fc2 mandrill_puzzle learn_dump 3 3
 samples/sync.sh
 
 ./strips.py fc2 hanoi learn_dump 3
@@ -14,8 +14,8 @@ samples/sync.sh
 ./strips.py fc2 xhanoi learn_dump 4
 samples/sync.sh
 
-./strips.py fc2 digital_lightsout learn_dump
+./strips.py fc2 digital_lightsout learn_dump 4
 samples/sync.sh
-./strips.py fc digital_lightsout_skewed learn_dump
+./strips.py fc digital_lightsout_skewed learn_dump 3
 samples/sync.sh
 
