dc22c919f4d4a1bb5f5b46a2b9156679fe4a213e
Masataro Asai <guicho2.71828@gmail.com>
2017-05-12 15:46:29 +0900

    fix dump_all_bits

--- dump_all_bits.py
+++ dump_all_bits.py
@@ -5,14 +5,14 @@ from model import GumbelAE, ConvolutionalGumbelAE
 from plot import plot_grid, plot_grid2
 
 def run(ae):
-    m = 16
+    m = ae.parameters['M']
     zs = (((np.arange(2**m)[:,None] & (1 << np.arange(m)))) > 0).astype(int)
     ys = ae.decode_binary(zs)
     per_image = 2**8
-    for j in range((2**16) // per_image):
+    for j in range((2**m) // per_image):
         path = ae.local("all-bits{}.png".format(j))
         print(path)
-        plot_grid2(ys[j*per_image:(1+j)*per_image],w=16,path=path)
+        plot_grid2(ys[j*per_image:(1+j)*per_image],w=m,path=path)
 
-run(GumbelAE("samples/mnist_puzzle32_model/"))
-run(GumbelAE("samples/mnist_puzzle32p_model/"))
+run(GumbelAE("samples/digital_lightsout_4_fc2/"))
+
