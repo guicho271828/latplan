9fd7895e334ae4d1fe0ad7eb581c1e6045756cf0
Masataro Asai <guicho2.71828@gmail.com>
2017-05-15 11:31:50 +0900

    dump_all_bits is impractical...?

--- dump_all_bits.py
+++ dump_all_bits.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from model import GumbelAE, ConvolutionalGumbelAE
+from model import GumbelAE, GumbelAE2, ConvolutionalGumbelAE
 from plot import plot_grid, plot_grid2
 
 def run(ae):
-    m = ae.parameters['M']
-    zs = (((np.arange(2**m)[:,None] & (1 << np.arange(m)))) > 0).astype(int)
+    ae.load()
+    n = ae.parameters['N']
+    zs = (((np.arange(2**n)[:,None] & (1 << np.arange(n)))) > 0).astype(int)
     ys = ae.decode_binary(zs)
     per_image = 2**8
-    for j in range((2**m) // per_image):
+    for j in range((2**n) // per_image):
         path = ae.local("all-bits{}.png".format(j))
         print(path)
-        plot_grid2(ys[j*per_image:(1+j)*per_image],w=m,path=path)
+        plot_grid2(ys[j*per_image:(1+j)*per_image],w=n,path=path)
 
-run(GumbelAE("samples/digital_lightsout_4_fc2/"))
+# run(GumbelAE2("samples/digital_lightsout_4_fc2"))
+# run(GumbelAE( "samples/digital_lightsout_skewed_3_fc"))
+# run(GumbelAE2("samples/hanoi3_fc2"))
+# run(GumbelAE2("samples/hanoi4_fc2"))
+# run(GumbelAE2("samples/mandrill_puzzle33_fc2"))
+# run(GumbelAE2("samples/mnist_puzzle33_fc2"))
+# run(GumbelAE2("samples/xhanoi4_fc2"))
+
 
