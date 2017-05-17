73b0882bfa58e4698008a68025d34a3b8ca7175e
Masataro Asai <guicho2.71828@gmail.com>
2017-05-17 17:59:30 +0900

    fix experiment path

--- experiments/mands-lightsout.sh
+++ experiments/mands-lightsout.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./plan.py mands 'run_lightsout ( "samples/digital_lightsout_fc2" ,"fc2", import_module("puzzles.digital_lightsout" ) )' |& tee $(dirname $0)/mands-lightsout.log
+./plan.py mands 'run_lightsout ( "samples/digital_lightsout_4_fc2" ,"fc2", import_module("puzzles.digital_lightsout" ) )' |& tee $(dirname $0)/mands-lightsout.log
