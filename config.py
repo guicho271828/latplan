44a5d76938964587f8847f0c27c4aac69337b1d6
Masataro Asai <guicho2.71828@gmail.com>
2017-06-30 21:13:08 +0900

    misc usability refactroing
    * dumping methods
    * sort the results for reporting
    * log readability
    * use full_epoch for computing the annealing ratio
    * dump image when the best parameter is found
    * free GPU memory after each run
    * print the current steps / total number of steps for grid search

--- config.py
+++ config.py
@@ -5,11 +5,20 @@ import keras.backend as K
 tf.logging.set_verbosity(tf.logging.ERROR)
 # K.set_floatx('float16')
 print("Default float: {}".format(K.floatx()))
-K.set_session(
-    tf.Session(
-        config=tf.ConfigProto(
-            allow_soft_placement=True,
-            gpu_options =
-            tf.GPUOptions(
-                per_process_gpu_memory_fraction=1.0,
-                allow_growth=True,))))
+
+def load_session():
+    K.set_session(
+        tf.Session(
+            config=tf.ConfigProto(
+                allow_soft_placement=True,
+                gpu_options =
+                tf.GPUOptions(
+                    per_process_gpu_memory_fraction=1.0,
+                    allow_growth=True,))))
+
+load_session()
+clear_session = K.clear_session
+
+def reload_session():
+    clear_session()
+    load_session()
