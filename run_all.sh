a114dc84ffb9371ce404c4102c892d0b2f4a36ee
Masataro Asai <guicho2.71828@gmail.com>
2017-06-30 15:32:08 +0900

    stop calling samples/sync.sh

--- run_all.sh
+++ run_all.sh
@@ -3,19 +3,13 @@
 trap exit SIGINT
 
 ./strips.py fc2 mnist_puzzle learn_dump 3 3
-samples/sync.sh
 ./strips.py fc2 mandrill_puzzle learn_dump 3 3
-samples/sync.sh
 
 ./strips.py fc2 hanoi learn_dump 3
-samples/sync.sh
 ./strips.py fc2 hanoi learn_dump 4
-samples/sync.sh
 ./strips.py fc2 xhanoi learn_dump 4
-samples/sync.sh
 
 ./strips.py fc2 digital_lightsout learn_dump 4
-samples/sync.sh
 ./strips.py fc digital_lightsout_skewed learn_dump 3
-samples/sync.sh
+
 
