44a776dc8eb4aad631f1c900b5dc662b7327c78c
Masataro Asai <guicho2.71828@gmail.com>
2017-07-08 11:03:29 +0900

    tune; 25 bits

--- strips.py
+++ strips.py
@@ -30,8 +30,8 @@ default_parameters = {
     'batch_size'      : 2000,
     'full_epoch'      : 1000,
     'epoch'           : 1000,
-    'max_temperature' : 2.0,
-    'min_temperature' : 0.1,
+    'max_temperature' : 5.0,
+    'min_temperature' : 0.7,
 }
 parameters = {}
 
@@ -270,20 +270,20 @@ def run(learn,*args, **kwargs):
     return ae
 
 
+parameters = {
+    'layer'      :[2000],# [400,4000],
+    'clayer'     :[16],# [400,4000],
+    'dropout'    :[0.4], #[0.1,0.4],
+    'N'          :[25],  #[25,49],
+    'dropout_z'  :[False],
+    'activation' : ['tanh'],
+    'full_epoch' :[500],
+    'epoch'      :[500],
+    'batch_size' :[2000],
+    'lr'         :[0.001],
+}
+
 def mnist_puzzle(width=3,height=3):
-    global parameters
-    parameters = {
-        'layer'      :[2000],# [400,4000],
-        'clayer'     :[16],# [400,4000],
-        'dropout'    :[0.4], #[0.1,0.4],
-        'N'          :[28],  #[25,49],
-        'dropout_z'  :[False],
-        'activation' : ['tanh'],
-        'full_epoch' :[1000],
-        'epoch'      :[500],
-        'batch_size' :[2000],
-        'lr'         :[0.001],
-    }
     import puzzles.mnist_puzzle as p
     configs = p.generate_configs(width*height)
     configs = np.array([ c for c in configs ])
@@ -324,19 +324,6 @@ def random_mnist_puzzle(width=3,height=3):
     dump_all_actions(ae,configs,        lambda configs: p.transitions(width,height,configs),)
     
 def lenna_puzzle(width=3,height=3):
-    global parameters
-    parameters = {
-        'layer'      :[1000],# [400,4000],
-        'clayer'     :[4],# [400,4000],
-        'dropout'    :[0.1], #[0.1,0.4],
-        'N'          :[36],  #[25,49],   # learn correctly with 36 bits; not with lower bits
-        'dropout_z'  :[False],
-        'activation' : ['tanh'],
-        'full_epoch' :[1000],
-        'epoch'      :[500],
-        'batch_size' :[2000],
-        'lr'         :[0.001],
-    }
     import puzzles.lenna_puzzle as p
     configs = p.generate_configs(width*height)
     configs = np.array([ c for c in configs ])
@@ -352,19 +339,6 @@ def lenna_puzzle(width=3,height=3):
     dump_all_actions(ae,configs,        lambda configs: p.transitions(width,height,configs),)
 
 def mandrill_puzzle(width=3,height=3):
-    global parameters
-    parameters = {
-        'layer'      :[2000],# [400,4000],
-        'clayer'     :[16],# [400,4000],
-        'dropout'    :[0.4], #[0.1,0.4],
-        'N'          :[28],  #[25,49],
-        'dropout_z'  :[False],
-        'activation' : ['tanh'],
-        'full_epoch' :[1000],
-        'epoch'      :[500],
-        'batch_size' :[2000],
-        'lr'         :[0.0001],
-    }
     import puzzles.mandrill_puzzle as p
     configs = p.generate_configs(width*height)
     configs = np.array([ c for c in configs ])
@@ -382,130 +356,16 @@ def mandrill_puzzle(width=3,height=3):
 def hanoi_hard(disks=5):
     global parameters
     parameters = {
-        # 'layer'      :[1000,2000,4000],
-        # 'dropout'    :[0.1,0.2,0.4],
-        # 'N'          :[16,25,30,36],
-        # 'batch_size' :[100],
-        # 'dropout_z'  :[True,False],
-        # short results
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 30, 'layer': 4000, 'error': -0.640392005443573}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 30, 'layer': 4000, 'error': -0.63293421268463135}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 30, 'layer': 2000, 'error': -0.62783586978912354}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 36, 'layer': 4000, 'error': -0.62599468231201172}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 30, 'layer': 2000, 'error': -0.62438416481018066}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 36, 'layer': 2000, 'error': -0.62357735633850098}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 30, 'layer': 2000, 'error': -0.61835479736328125}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 36, 'layer': 2000, 'error': -0.61645066738128662}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 36, 'layer': 4000, 'error': -0.61477929353713989}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 25, 'layer': 4000, 'error': -0.60899567604064941}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 25, 'layer': 4000, 'error': -0.60734283924102783}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 36, 'layer': 2000, 'error': -0.60594701766967773}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 30, 'layer': 4000, 'error': -0.60450851917266846}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 36, 'layer': 4000, 'error': -0.60306370258331299}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 36, 'layer': 4000, 'error': -0.60291892290115356}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 36, 'layer': 2000, 'error': -0.59930217266082764}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 25, 'layer': 2000, 'error': -0.59898924827575684}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 30, 'layer': 4000, 'error': -0.5976942777633667}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 30, 'layer': 2000, 'error': -0.59667587280273438}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 25, 'layer': 4000, 'error': -0.59463036060333252}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 25, 'layer': 2000, 'error': -0.59247839450836182}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 30, 'layer': 4000, 'error': -0.59211128950119019}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 36, 'layer': 4000, 'error': -0.59196054935455322}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 30, 'layer': 4000, 'error': -0.59061408042907715}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 30, 'layer': 2000, 'error': -0.59042090177536011}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 16, 'layer': 4000, 'error': -0.58864903450012207}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 25, 'layer': 2000, 'error': -0.58647298812866211}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 25, 'layer': 2000, 'error': -0.58424681425094604}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 36, 'layer': 1000, 'error': -0.58421194553375244}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 30, 'layer': 1000, 'error': -0.58234208822250366}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 36, 'layer': 1000, 'error': -0.58037769794464111}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 36, 'layer': 4000, 'error': -0.57998448610305786}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 16, 'layer': 4000, 'error': -0.57961761951446533}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 36, 'layer': 2000, 'error': -0.57958716154098511}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 16, 'layer': 4000, 'error': -0.57866716384887695}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 16, 'layer': 2000, 'error': -0.57798409461975098}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 25, 'layer': 1000, 'error': -0.57733619213104248}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 36, 'layer': 2000, 'error': -0.5758746862411499}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 16, 'layer': 2000, 'error': -0.57536393404006958}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 30, 'layer': 1000, 'error': -0.57484549283981323}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 25, 'layer': 1000, 'error': -0.57483327388763428}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 25, 'layer': 2000, 'error': -0.5738484263420105}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 16, 'layer': 4000, 'error': -0.57321882247924805}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 16, 'layer': 1000, 'error': -0.57276612520217896}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 36, 'layer': 1000, 'error': -0.57263761758804321}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 25, 'layer': 1000, 'error': -0.57030832767486572}
-        # {'dropout': 0.1 , 'dropout_z': True, 'N': 16, 'layer': 2000, 'error': -0.56961214542388916}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 25, 'layer': 4000, 'error': -0.56890702247619629}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 30, 'layer': 1000, 'error': -0.56395626068115234}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 16, 'layer': 1000, 'error': -0.56385207176208496}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 25, 'layer': 4000, 'error': -0.562419593334198}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 36, 'layer': 1000, 'error': -0.55937016010284424}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 25, 'layer': 1000, 'error': -0.55886197090148926}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 25, 'layer': 2000, 'error': -0.55770504474639893}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 16, 'layer': 4000, 'error': -0.55719602108001709}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 16, 'layer': 2000, 'error': -0.55505228042602539}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 16, 'layer': 1000, 'error': -0.55492079257965088}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 36, 'layer': 1000, 'error': -0.5539441704750061}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 16, 'layer': 1000, 'error': -0.55313944816589355}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 16, 'layer': 2000, 'error': -0.55175542831420898}
-        # {'dropout': 0.4 , 'dropout_z': False, 'N': 25, 'layer': 1000, 'error': -0.55133038759231567}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 16, 'layer': 4000, 'error': -0.54752600193023682}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 30, 'layer': 2000, 'error': -0.54715025424957275}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 30, 'layer': 1000, 'error': -0.54615998268127441}
-        # {'dropout': 0.2 , 'dropout_z': False, 'N': 30, 'layer': 1000, 'error': -0.54047298431396484}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 16, 'layer': 1000, 'error': -0.53436446189880371}
-        # {'dropout': 0.2 , 'dropout_z': True, 'N': 16, 'layer': 1000, 'error': -0.53258365392684937}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 30, 'layer': 1000, 'error': -0.53125882148742676}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 16, 'layer': 2000, 'error': -0.5290108323097229}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 36, 'layer': 1000, 'error': -0.5274394154548645}
-        # {'dropout': 0.4 , 'dropout_z': True, 'N': 25, 'layer': 1000, 'error': -0.51308572292327881}
-        # {'dropout': 0.1 , 'dropout_z': False, 'N': 25, 'layer': 4000, 'error': -0.50917017459869385}
-
-        # pruned bad cases
-        # 'layer'      :[2000,4000],
-        # 'dropout'    :[0.2,0.4],
-        # 'N'          :[25,30,36],
-        # 'batch_size' :[100],
-        # 'dropout_z'  :[True,False],
-        # middle results (500/1000 epoch)
-        # {'dropout': 0.4, 'error': -0.60835909843444824, 'layer': 4000, 'N': 25,'dropout_z': False}
-        # {'dropout': 0.2, 'error': -0.6075860857963562,  'layer': 2000, 'N': 36,'dropout_z': True}
-        # {'dropout': 0.4, 'error': -0.60412561893463135, 'layer': 4000, 'N': 36,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.59957921504974365, 'layer': 2000, 'N': 30,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.59358477592468262, 'layer': 2000, 'N': 30,'dropout_z': False}
-        # {'dropout': 0.2, 'error': -0.59129548072814941, 'layer': 4000, 'N': 25,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.58963090181350708, 'layer': 2000, 'N': 30,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.58892679214477539, 'layer': 4000, 'N': 30,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.58624380826950073, 'layer': 4000, 'N': 36,'dropout_z': False}
-        # {'dropout': 0.2, 'error': -0.58586376905441284, 'layer': 4000, 'N': 30,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.58075928688049316, 'layer': 2000, 'N': 25,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.57950294017791748, 'layer': 4000, 'N': 25,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.57386302947998047, 'layer': 4000, 'N': 30,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.57242345809936523, 'layer': 2000, 'N': 36,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.55973660945892334, 'layer': 4000, 'N': 25,'dropout_z': True}
-        # {'dropout': 0.4, 'error': -0.55722737312316895, 'layer': 4000, 'N': 36,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.55417418479919434, 'layer': 2000, 'N': 30,'dropout_z': True}
-        # {'dropout': 0.4, 'error': -0.55290007591247559, 'layer': 4000, 'N': 30,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.55126595497131348, 'layer': 2000, 'N': 36,'dropout_z': True}
-        # {'dropout': 0.2, 'error': -0.54389727115631104, 'layer': 2000, 'N': 25,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.51387894153594971, 'layer': 2000, 'N': 25,'dropout_z': False}
-        # {'dropout': 0.4, 'error': -0.49873256683349609, 'layer': 2000, 'N': 25,'dropout_z': True}
-
-        # 'layer'      :[4000], #[1000,2000,4000]
-        # 'dropout'    :[0.4],  #[0.1,0.2,0.4]
-        # 'N'          :[30],   #[16,25,30,36],
-        # 'batch_size' :[100],
-        # 'dropout_z'  :[False],#[True,False],
-
-        'layer'      :[5000], #[1000,2000,4000]
-        'dropout'    :[0.6],  #[0.1,0.2,0.4]
-        'N'          :[36],   #[16,25,30,36],
-        'batch_size' :[200],
-        'dropout_z'  :[False],#[True,False],
-
+        'layer'      :[2000],# [400,4000],
+        'clayer'     :[16],# [400,4000],
+        'dropout'    :[0.4], #[0.1,0.4],
+        'N'          :[25],  #[25,49],
+        'dropout_z'  :[False],
+        'activation' : ['tanh'],
         'full_epoch' :[1000],
-        # quick eval
         'epoch'      :[500],
+        'batch_size' :[2000],
+        'lr'         :[0.001],
     }
     import puzzles.hanoi as p
     configs = p.generate_configs(disks)
