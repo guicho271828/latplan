b7b9191cdc72dca4190638203c1b5bd3d98d4cb7
Masataro Asai <guicho2.71828@gmail.com>
2017-07-11 14:37:43 +0900

    problem instance generator for trivial-planner

---
+++ trivial-planner-instances/example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+./trivial-planner.py samples/puzzle_mnist33_fc/ \
+                     trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/init.png \
+                     trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/goal.png
