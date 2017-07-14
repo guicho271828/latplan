b2225f4fec86865486bbd96b0b508011c4cd0704
Masataro Asai <guicho2.71828@gmail.com>
2017-07-14 11:27:14 +0900

    sync deletes old files

--- samples/sync.sh
+++ samples/sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo  $(dirname $0)
-rsync -av --include '*/' --include '*.png' --include '*.log' --exclude '*' $(dirname $0)/ ~/Dropbox/FukunagaLabShare/OngoingWorks/Asai/latent-planner/sync/
+rsync -av --include '*/' --include '*.png' --include '*.log' --exclude '*' \
+      --delete \
+      $(dirname $0)/ ~/Dropbox/FukunagaLabShare/OngoingWorks/Asai/latent-planner/sync/
