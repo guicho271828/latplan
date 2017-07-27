b8c6a2f8ae2a54b99c1437a791c0e1f7f81e1a06
Masataro Asai <guicho2.71828@gmail.com>
2017-07-27 10:41:13 +0900

    number of towers in hanoi is configurable

--- latplan/puzzles/hanoi.py
+++ latplan/puzzles/hanoi.py
@@ -1,6 +1,8 @@
 
 import numpy as np
-from .model.hanoi import generate_configs, successors, config_state, state_config
+from .model.hanoi import generate_configs, successors, config_state
+
+## patterns ##############################################################
 
 patterns = [
     [[0,0,0,0,0,0,0,0,],
@@ -119,54 +121,65 @@ patterns = [
 
 patterns = np.array(patterns)
 
-def generate1(config):
+## code ##############################################################
+
+def generate1(config,disks,towers):
     l = len(config)
-    disk_size = 8
-    disk_inc = disk_size // 2
-    disk_height = disk_size
+    disk_height = 8
+    disk_inc = disk_height // 2
     base_disk_width_factor = 2
-    base_disk_width = disk_size * base_disk_width_factor
-    figure = np.ones([l*disk_height,(l*2*disk_inc+base_disk_width)*3+2],dtype=np.int8)
-    state = config_state(config)
+    base_disk_width = disk_height * base_disk_width_factor
+
+    border = 1
+    tower_width  = disks * (2*disk_inc) + base_disk_width + border
+    tower_height = disks*disk_height
+    
+    figure = np.ones([tower_height,
+                      tower_width*towers+2],dtype=np.int8)
+    state = config_state(config,disks,towers)
     # print(l,figure.shape)
     # print(config)
     # print(state)
     for i, tower in enumerate(state):
         tower.reverse()
         # print(i,tower)
-        x_left  = (l*2*disk_inc+base_disk_width)*i     +   i
-        x_right = (l*2*disk_inc+base_disk_width)*(i+1) + (i+1) - 1
+        x_center = tower_width *  i + disks * disk_inc # lacks base_disk_width
+        
+        x_left  = tower_width *  i
+        x_right = tower_width * (i+1) - border
         for j,disk in enumerate(tower):
             # print(j,disk,(l-j)*2)
             figure[
-                (l-j-1)*disk_height:(l-j)*disk_height,
-                x_left + (l-disk)*disk_inc : x_right - (l-disk)*disk_inc] \
+                tower_height - disk_height * (j+1) :
+                tower_height - disk_height * j,
+                x_center - disk * disk_inc :
+                x_center + disk * disk_inc + base_disk_width] \
                 = 0
                 # = np.tile(patterns[disk],(1,disk+base_disk_width_factor))
     return figure
 
-def generate(configs):
-    return np.array([ generate1(c) for c in configs ])
+def generate(configs,disks,towers):
+    return np.array([ generate1(c,disks,towers) for c in configs ])
                 
 
-def states(size, configs=None):
+def states(disks, towers, configs=None):
     if configs is None:
-        configs = generate_configs(size)
-    return generate(configs)
+        configs = generate_configs(disks, towers)
+    return generate(configs,disks,towers)
 
-def transitions(size, configs=None, one_per_state=False):
+def transitions(disks, towers, configs=None, one_per_state=False):
     if configs is None:
-        configs = generate_configs(size)
+        configs = generate_configs(disks, towers)
     if one_per_state:
         def pickone(thing):
             index = np.random.randint(0,len(thing))
             return thing[index]
         transitions = np.array([
-            generate([c1,pickone(successors(c1))])
+            generate([c1,pickone(successors(c1,disks,towers))],disks,towers)
             for c1 in configs ])
     else:
-        transitions = np.array([ generate([c1,c2])
-                                 for c1 in configs for c2 in successors(c1) ])
+        transitions = np.array([ generate([c1,c2],disks,towers)
+                                 for c1 in configs for c2 in successors(c1,disks,towers) ])
     return np.einsum('ab...->ba...',transitions)
 
 
--- tests/hanoi.py
+++ tests/hanoi.py
@@ -8,14 +8,16 @@ from latplan.puzzles.hanoi import generate_configs, successors, generate, states
 
 from plot import plot_image, plot_grid
 
-disks = 6
-configs = generate_configs(disks)
-puzzles = generate(configs)
+disks = 8
+towers = 4
+
+configs = generate_configs(disks,towers)
+puzzles = generate(configs,disks,towers)
 print(puzzles.shape)
 print(puzzles[10])
 plot_image(puzzles[0],"hanoi.png")
 plot_grid(puzzles[:36],"hanois.png")
-_transitions = transitions(disks)
+_transitions = transitions(disks,towers)
 print(_transitions.shape)
 import numpy.random as random
 indices = random.randint(0,_transitions[0].shape[0],18)
