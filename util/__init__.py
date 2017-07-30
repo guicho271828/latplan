d2c76ad40addeb879730d21c904f88f4d226c207
Masataro Asai <guicho2.71828@gmail.com>
2017-07-30 09:44:52 +0900

    prepare_binary_classification_data

--- util/__init__.py
+++ util/__init__.py
@@ -14,3 +14,24 @@ def get_ae_type(directory):
 
 def curry(fn,*args1,**kwargs1):
     return lambda *args,**kwargs: fn(*args1,*args,**{**kwargs1,**kwargs})
+
+def prepare_binary_classification_data(real, fake):
+    import numpy as np
+    shape = real.shape[1:]
+    
+    both = np.concatenate((real, fake),axis=0)
+    both = np.reshape(both, (len(both), -1)) # flatten
+
+    data_dim = both.shape[1]
+
+    both2 = np.pad(both, ((0,0),(0,1)), 'constant') # default 0
+    both2[:len(real),-1] = 1                        # tag true
+    np.random.shuffle(both2)
+
+    train_in  = np.reshape(both2[:int(0.9*len(both2)), :data_dim], (-1, *shape))
+    train_out = both2[:int(0.9*len(both2)), -1]
+    test_in   = np.reshape(both2[int(0.9*len(both2)):, :data_dim], (-1, *shape))
+    test_out  = both2[int(0.9*len(both2)):, -1]
+
+    return train_in, train_out, test_in, test_out
+
