e082105317ec81e90be10327c8acae3c554de222
Masataro Asai <guicho2.71828@gmail.com>
2017-08-02 08:57:42 +0900

    action_autoencoder takes the second argument

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,7 @@ trap exit SIGINT
 ./strips.py fc puzzle_mnist learn_plot_dump 3 3
 ./state_discriminator3.py samples/puzzle_mnist33_fc/ learn &
 ./action_discriminator.py samples/puzzle_mnist33_fc/ learn &
-./action_autoencoder.py samples/puzzle_mnist33_fc/ &
+./action_autoencoder.py   samples/puzzle_mnist33_fc/ learn &
 wait
 ./trivial-planner.py samples/puzzle_mnist33_fc/ trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/
 parallel --bar --eta --timeout 50 --joblog parallel.log "./trivial-planner.py samples/puzzle_mnist33_fc/ {1} > {1}/fc.log" ::: trivial-planner-instances/latplan.puzzles.puzzle_mnist/*
@@ -13,7 +13,7 @@ parallel --bar --eta --timeout 50 --joblog parallel.log "./trivial-planner.py sa
 ./strips.py conv puzzle_mnist learn_plot_dump 3 3
 ./state_discriminator3.py samples/puzzle_mnist33_conv/ learn &
 ./action_discriminator.py samples/puzzle_mnist33_conv/ learn &
-./action_autoencoder.py samples/puzzle_mnist33_conv/ &
+./action_autoencoder.py   samples/puzzle_mnist33_conv/ learn &
 wait
 ./trivial-planner.py samples/puzzle_mnist33_conv/ trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/
 
