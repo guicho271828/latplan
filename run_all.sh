be6afe81d0a8fe5fd60bcbdf8c925617784408ff
Masataro Asai <guicho2.71828@gmail.com>
2017-08-02 08:52:57 +0900

    update run_all

--- run_all.sh
+++ run_all.sh
@@ -4,20 +4,20 @@ trap exit SIGINT
 
 ./strips.py fc puzzle_mnist learn_plot_dump 3 3
 ./state_discriminator3.py samples/puzzle_mnist33_fc/ learn &
-./action_discriminator.py samples/puzzle_mnist33_fc/ &
+./action_discriminator.py samples/puzzle_mnist33_fc/ learn &
 ./action_autoencoder.py samples/puzzle_mnist33_fc/ &
 wait
 ./trivial-planner.py samples/puzzle_mnist33_fc/ trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/
-parallel --timeout 45 --joblog parallel.log "./trivial-planner.py samples/puzzle_mnist33_fc/ {1} > {1}/fc.log" ::: trivial-planner-instances/latplan.puzzles.puzzle_mnist/*
+parallel --bar --eta --timeout 50 --joblog parallel.log "./trivial-planner.py samples/puzzle_mnist33_fc/ {1} > {1}/fc.log" ::: trivial-planner-instances/latplan.puzzles.puzzle_mnist/*
 
 ./strips.py conv puzzle_mnist learn_plot_dump 3 3
 ./state_discriminator3.py samples/puzzle_mnist33_conv/ learn &
-./action_discriminator.py samples/puzzle_mnist33_conv/ &
+./action_discriminator.py samples/puzzle_mnist33_conv/ learn &
 ./action_autoencoder.py samples/puzzle_mnist33_conv/ &
 wait
 ./trivial-planner.py samples/puzzle_mnist33_conv/ trivial-planner-instances/latplan.puzzles.puzzle_mnist/0-0/
 
-parallel --timeout 45 --joblog parallel.log "./trivial-planner.py samples/puzzle_mnist33_conv/ {1} > {1}/conv.log" ::: trivial-planner-instances/latplan.puzzles.puzzle_mnist/*
+parallel --bar --eta --timeout 50 --joblog parallel.log "./trivial-planner.py samples/puzzle_mnist33_conv/ {1} > {1}/conv.log" ::: trivial-planner-instances/latplan.puzzles.puzzle_mnist/*
 
 ./strips.py fc puzzle_mandrill learn_plot_dump 3 3
 ./strips.py fc puzzle_lenna learn_plot_dump 3 3
