de263394d4c3064927a0797d6b55cc650ee330dd
Masataro Asai <guicho2.71828@gmail.com>
2017-08-06 14:35:37 +0900

    tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-parallel -j 4 python3 ::: ./counter_mnist.py ./counter_random_mnist.py ./digital_lightsout.py ./digital_lightsout_skewed.py ./hanoi.py ./puzzle_digital.py ./puzzle_lenna.py ./puzzle_mandrill.py ./puzzle_spider.py ./puzzle_wrong.py
+parallel -j 4 python3 ::: \
+         ./counter_*.py \
+         ./digital_lightsout*.py \
+         ./hanoi.py
+
+for py in ./puzzle_*.py ; do
+    $py
+done
+
 
