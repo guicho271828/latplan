8fedf666fd947ead67a1a4dcd6a0cef32b7f8e5d
Masataro Asai <guicho2.71828@gmail.com>
2017-08-06 12:00:04 +0900

    [gpu] test bold

--- tests/puzzle_mnist_validate.py
+++ tests/puzzle_mnist_validate.py
@@ -17,21 +17,27 @@ import itertools
 c = [ c for c in itertools.islice(p.generate_configs(9), 10000) ]
 s = p.states(3,3,c)
 
+
+from colors import color
+from functools import partial
+
+style = partial(color, fg='black', bg='white')
+
 from latplan.util.timer import Timer
-with Timer("************************* validate_states on cpu ***************************"):
+with Timer(style("************************* validate_states on cpu ***************************")):
     assert np.all(m.validate_states_cpu(s,3,3))
 
-with Timer("************************* validate_states on gpu, batch=100 ***************************"):
+with Timer(style("************************* validate_states on gpu, batch=100 ***************************")):
     assert np.all(m.validate_states_gpu(s,3,3,batch_size=100))
 
-with Timer("************************* validate_states on gpu, batch=1000 ***************************"):
+with Timer(style("************************* validate_states on gpu, batch=1000 ***************************")):
     assert np.all(m.validate_states_gpu(s,3,3,batch_size=1000))
 
-with Timer("************************* to_configs on cpu ***************************"):
+with Timer(style("************************* to_configs on cpu ***************************")):
     print(m.to_configs_cpu(s,3,3)[:3])
 
-with Timer("************************* to_configs on gpu, batch=100 ***************************"):
+with Timer(style("************************* to_configs on gpu, batch=100 ***************************")):
     print(m.to_configs_gpu(s,3,3,batch_size=100)[:3])
 
-with Timer("************************* to_configs on gpu, batch=1000 ***************************"):
+with Timer(style("************************* to_configs on gpu, batch=1000 ***************************")):
     print(m.to_configs_gpu(s,3,3,batch_size=1000)[:3])
