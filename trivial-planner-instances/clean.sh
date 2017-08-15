8e516dcdb3c09d500bf59b4158640613b425b6ac
Masataro Asai <guicho2.71828@gmail.com>
2017-08-15 09:59:52 +0900

    fix generator scripts: even-steps plans are unreachable!

--- trivial-planner-instances/clean.sh
+++ trivial-planner-instances/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -v $(dirname $0)/*/*/*.log
-rm -v $(dirname $0)/*/*/path*
-rm -v $(dirname $0)/*/*/init_goal_reconstruction*
+rm -v $(dirname $0)/*/*/*path*
+rm -v $(dirname $0)/*/*/*reconstruction*
