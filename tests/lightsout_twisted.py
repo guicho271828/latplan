b6fd4713dbc49c01569df44afa422e4fbd36d360
Masataro Asai <guicho2.71828@gmail.com>
2017-08-15 21:43:05 +0900

    tests: lightsout_twisted transitions

--- tests/lightsout_twisted.py
+++ tests/lightsout_twisted.py
@@ -11,16 +11,13 @@ from plot import plot_image, plot_grid
 configs = generate_configs(3)
 puzzles = generate([configs[-1]])
 plot_image(puzzles[0],"lightsout_twisted.png")
-# puzzles = generate(configs)
-# plot_image(puzzles[10],"lightsout_twisted.png")
-# plot_grid(puzzles[:36],"lightsout_twisteds.png")
-# _transitions = transitions(3)
-# import numpy.random as random
-# indices = random.randint(0,_transitions[0].shape[0],18)
-# _transitions = _transitions[:,indices]
-# print(_transitions.shape)
-# transitions_for_show = \
-#     np.einsum('ba...->ab...',_transitions) \
-#       .reshape((-1,)+_transitions.shape[2:])
-# print(transitions_for_show.shape)
-# plot_grid(transitions_for_show,"lightsout_twisted_transitions.png")
+_transitions = transitions(3, configs=configs[:2])
+import numpy.random as random
+indices = random.randint(0,_transitions[0].shape[0],18)
+_transitions = _transitions[:,indices]
+print(_transitions.shape)
+transitions_for_show = \
+    np.einsum('ba...->ab...',_transitions) \
+      .reshape((-1,)+_transitions.shape[2:])
+print(transitions_for_show.shape)
+plot_grid(transitions_for_show,"lightsout_twisted_transitions.png")
