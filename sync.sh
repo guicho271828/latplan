d0566f0580344a31d1d051da3c013dcd9ed77488
Masataro Asai <guicho2.71828@gmail.com>
2017-08-21 10:33:18 +0900

    also sync *.valid

--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@
 echo  $(dirname $0)
 rsync -av \
       -m \
-      --include '*/' --include '*.png' --include '*.log' --exclude '*' \
+      --include '*/' --include '*.png' --include '*.log' --include '*.valid' --exclude '*' \
       --delete \
       $(dirname $0)/ ~/Dropbox/FukunagaLabShare/OngoingWorks/Asai/latent-planner/sync/
