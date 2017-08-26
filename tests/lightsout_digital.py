51012879cee5c23895f11c4171ebe2cf8f2aa370
Masataro Asai <guicho2.71828@gmail.com>
2017-08-26 11:55:36 +0900

    [validator] lightsout_digital

--- tests/lightsout_digital.py
+++ tests/lightsout_digital.py
@@ -4,15 +4,18 @@ import numpy as np
 import sys
 sys.path.append('../../')
 
-from latplan.puzzles.lightsout_digital import generate_configs, successors, generate, states, transitions
+import latplan.puzzles.lightsout_digital as p
 
 from plot import plot_image, plot_grid
 
-configs = generate_configs(3)
-puzzles = generate(configs)
+configs = p.generate_configs(3)
+puzzles = p.generate_cpu(configs)
 print(puzzles[10])
-plot_image(puzzles[10],"lightsout_digital.png")
-_transitions = transitions(3)
+plot_image(puzzles[10],"lightsout_digital_cpu.png")
+puzzles = p.generate_gpu(configs)
+print(puzzles[10])
+plot_image(puzzles[10],"lightsout_digital_gpu.png")
+_transitions = p.transitions(3, configs=configs[:100], one_per_state=True)
 import numpy.random as random
 indices = random.randint(0,_transitions[0].shape[0],18)
 _transitions = _transitions[:,indices]
---
+++ tests/lightsout_digital_validate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import importlib
+import numpy as np
+
+import latplan
+import latplan.puzzles.lightsout_digital as p
+
+# importlib.reload(p)
+
+import itertools
+c = [ c for c in itertools.islice(p.generate_configs(3), 10000) ]
+
+from colors import color
+from functools import partial
+
+style = partial(color, fg='black', bg='white')
+
+from latplan.util.timer import Timer
+
+with Timer(style("************************* states on cpu ***************************")):
+    s = p.generate_cpu(c)
+    print(s[120])
+
+with Timer(style("************************* states on gpu, batch=100  ***************************")):
+    s = p.generate_gpu(c, batch_size=100)
+    print(s[120])
+
+with Timer(style("************************* states on gpu, batch=1000 ***************************")):
+    s = p.generate_gpu(c, batch_size=1000)
+    print(s[120])
+
+with Timer(style("************************* validate_states on cpu ***************************")):
+    print("results:", np.all(p.validate_states(s)), "(should be True)")
+
+
+
+with Timer(style("************************* to_configs on gpu, batch=100 ***************************")):
+    p.to_configs(s,batch_size=100)
+    
+
+with Timer(style("************************* to_configs on gpu, batch=1000 ***************************")):
+    p.to_configs(s,batch_size=1000)
+
+print("config - to_configs(generate(config)):", np.sum(np.abs(c - p.to_configs(s,batch_size=1000))))
+print("original     :",c[120])
+print("reconstructed:",p.to_configs(s,batch_size=100)[120])
+
+c = c[:10]
+
+with Timer(style("************************* transitions ***************************")):
+    transitions = p.transitions(3,configs=c)
+
+with Timer(style("************************* transitions one_per_state ***************************")):
+    transitions = p.transitions(3,configs=c,one_per_state=True)
+
+with Timer(style("************************* validate_transitions_cpu ***************************")):
+    print("all transitions valid?:",np.all(p.validate_transitions(transitions,batch_size=1000)))
+
