1bb95c053dd69d60ef8d7d1b7513b8296314c952
Masataro Asai <guicho2.71828@gmail.com>
2017-08-26 11:19:10 +0900

    do not parallelize problem generation

--- problem-instances/example.sh
+++ problem-instances/example.sh
@@ -2,10 +2,9 @@
 
 trap exit SIGINT
 
-parallel ./generate.py 7 5 puzzle {} 3 3 ::: mandrill spider mnist
-# ./generate.py 7 5 puzzle mandrill 3 3
-# ./generate.py 7 5 puzzle spider   3 3
-# ./generate.py 7 5 puzzle mnist    3 3
+./generate.py 7 5 puzzle mandrill 3 3
+./generate.py 7 5 puzzle spider   3 3
+./generate.py 7 5 puzzle mnist    3 3
 
 ./generate.py 7 5 hanoi 7 4
 ./generate.py 7 5 lightsout digital 4
