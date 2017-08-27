953d4667771e95cd6685d9c1276006a8b4b8b38c
Masataro Asai <guicho2.71828@gmail.com>
2017-08-27 18:08:38 +0900

    problem generator can be run from anywhere

--- problem-instances/example.sh
+++ problem-instances/example.sh
@@ -2,10 +2,14 @@
 
 trap exit SIGINT
 
-./generate.py 7 100 puzzle mandrill 3 3
-./generate.py 7 100 puzzle spider   3 3
-./generate.py 7 100 puzzle mnist    3 3
+{
+    cd $(dirname $0)
 
-./generate.py 7 100 hanoi 7 4
-./generate.py 7 100 lightsout digital 4
-./generate.py 7 100 lightsout twisted 4
+    ./generate.py 7 100 puzzle mandrill 3 3
+    ./generate.py 7 100 puzzle spider   3 3
+    ./generate.py 7 100 puzzle mnist    3 3
+
+    ./generate.py 7 100 hanoi 7 4
+    ./generate.py 7 100 lightsout digital 4
+    ./generate.py 7 100 lightsout twisted 4
+}
