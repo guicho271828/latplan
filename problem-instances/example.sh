439572999596803d6c81dc4d11a09ab01a19a802
Masataro Asai <guicho2.71828@gmail.com>
2017-08-27 18:06:51 +0900

    100 instances

--- problem-instances/example.sh
+++ problem-instances/example.sh
@@ -2,10 +2,10 @@
 
 trap exit SIGINT
 
-./generate.py 7 5 puzzle mandrill 3 3
-./generate.py 7 5 puzzle spider   3 3
-./generate.py 7 5 puzzle mnist    3 3
+./generate.py 7 100 puzzle mandrill 3 3
+./generate.py 7 100 puzzle spider   3 3
+./generate.py 7 100 puzzle mnist    3 3
 
-./generate.py 7 5 hanoi 7 4
-./generate.py 7 5 lightsout digital 4
-./generate.py 7 5 lightsout twisted 4
+./generate.py 7 100 hanoi 7 4
+./generate.py 7 100 lightsout digital 4
+./generate.py 7 100 lightsout twisted 4
