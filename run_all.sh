532c0c3976c7f0cc3d77438e9925b8b0453a7f40
Masataro Asai <guicho2.71828@gmail.com>
2017-08-28 13:53:50 +0900

    do not use full cores

--- run_all.sh
+++ run_all.sh
@@ -4,32 +4,35 @@ ulimit -v 16000000000
 
 trap exit SIGINT
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.puzzle_mnist.log  \
+ncpu=$(cat /proc/cpuinfo | grep -c processor)
+jobs=$((ncpu-1))
+
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.puzzle_mnist.log  \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: puzzle_mnist_3_3_36_20000_conv \
          ::: problem-instances/latplan.puzzles.puzzle_mnist/*
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.puzzle_mandrill.log   \
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.puzzle_mandrill.log   \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: puzzle_mandrill_3_3_36_20000_conv \
          ::: problem-instances/latplan.puzzles.puzzle_mandrill/*
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.puzzle_spider.log   \
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.puzzle_spider.log   \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: puzzle_spider_3_3_36_20000_conv \
          ::: problem-instances/latplan.puzzles.puzzle_spider/*
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.hanoi.log   \
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.hanoi.log   \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: hanoi_7_4_36_10000_conv \
          ::: problem-instances/latplan.puzzles.hanoi/*
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.lightsout_digital.log   \
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.lightsout_digital.log   \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: lightsout_digital_4_36_20000_conv \
          ::: problem-instances/latplan.puzzles.lightsout_digital/*
 
-parallel --eta --timeout 90 --joblog latplan.puzzles.lightsout_twisted.log   \
+parallel -j $jobs --eta --timeout 90 --joblog latplan.puzzles.lightsout_twisted.log   \
          "./trivial-planner.py samples/{1} {2} Astar > {2}/{1}_Astar.log" \
          ::: lightsout_twisted_4_36_20000_conv \
          ::: problem-instances/latplan.puzzles.lightsout_twisted/*
