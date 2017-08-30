6414b5955c2802810361099002b5802f628337d5
Masataro Asai <guicho2.71828@gmail.com>
2017-08-30 11:00:19 +0900

    refactor state_discriminator3

--- state_discriminator3.py
+++ state_discriminator3.py
@@ -3,7 +3,7 @@ import warnings
 import config
 import numpy as np
 import latplan
-from latplan.model import PUDiscriminator, default_networks, combined_discriminate, combined_discriminate2
+from latplan.model import default_networks, combined_discriminate, combined_discriminate2
 from latplan.util        import curry, prepare_binary_classification_data, set_difference, bce
 from latplan.util.tuning import grid_search, nn_task
 
@@ -12,14 +12,6 @@ import tensorflow as tf
 
 float_formatter = lambda x: "%.3f" % x
 np.set_printoptions(formatter={'float_kind':float_formatter})
-
-################################################################
-# State discriminator.
-# 
-# I made this discriminator for pruning some state transitions while checking
-# the entire (2^98) transitions and make them compact.
-
-# negative examples (random bitstrings) are pre-filtered using SAE reconstruction
 
 inflation = 1
 
@@ -80,69 +72,32 @@ def prepare(data_valid, sae):
     train_in, train_out, test_in, test_out = prepare_binary_classification_data(data_valid, data_invalid)
     return train_in, train_out, test_in, test_out, data_valid, data_invalid
 
+sae = None
+cae = None
+discriminator = None
+combined = None
 
-# default values
-default_parameters = {
-    'lr'              : 0.0001,
-    'batch_size'      : 2000,
-    'full_epoch'      : 1000,
-    'epoch'           : 1000,
-    'max_temperature' : 2.0,
-    'min_temperature' : 0.1,
-    'M'               : 2,
-    'min_grad'        : 0.0,
-}
+def learn(method):
+    global cae, discriminator
+    default_parameters = {
+        'lr'              : 0.0001,
+        'batch_size'      : 2000,
+        'full_epoch'      : 1000,
+        'epoch'           : 1000,
+        'max_temperature' : 2.0,
+        'min_temperature' : 0.1,
+        'M'               : 2,
+        'min_grad'        : 0.0,
+    }
+    data_valid = np.loadtxt(sae.local("states.csv"),dtype=np.int8)
+    train_in, train_out, test_in, test_out, data_valid, data_invalid = prepare(data_valid,sae)
+    sae.plot_autodecode(data_invalid[:8], "_sd3/fake_samples.png")
 
-
-if __name__ == '__main__':
-    import numpy.random as random
-
-    import sys
-    if len(sys.argv) == 1:
-        sys.exit("{} [directory] [mode=learn]".format(sys.argv[0]))
-
-    directory    = sys.argv[1]
-    mode         = sys.argv[2]
-    directory_sd = "{}/_sd3/".format(directory)
-    import subprocess
-    subprocess.call(["mkdir","-p",directory_sd])
-    
-    from latplan.util import get_ae_type
-    aetype = get_ae_type(directory)
-    sae = default_networks[aetype](directory).load()
- 
-    if 'learn' in mode:
-        data_valid = np.loadtxt(sae.local("states.csv"),dtype=np.int8)
-
-        train_in, train_out, test_in, test_out, data_valid, data_invalid = prepare(data_valid,sae)
-
-        sae.plot_autodecode(data_invalid[:8], "_sd3/fake_samples.png")
-
+    if method is "feature":
+        # decode into image, extract features and learn from it
         train_image, test_image = sae.decode_binary(train_in), sae.decode_binary(test_in)
-        
-        if 'conv' in aetype:
-            train_in2, test_in2 = sae.get_features(train_image), sae.get_features(test_image)
-        else:
-            cae,_,_ = grid_search(curry(nn_task, default_networks['SimpleCAE'],
-                                        sae.local("_cae"),
-                                        train_image, train_image, test_image, test_image),
-                                  default_parameters,
-                                  {
-                                      'num_layers' :[2],
-                                      'layer'      :[500],
-                                      'clayer'     :[16],
-                                      'dropout'    :[0.4],
-                                      'batch_size' :[4000],
-                                      'full_epoch' :[1000],
-                                      'activation' :['relu'],
-                                      'epoch'      :[30],
-                                      'lr'         :[0.001],
-                                  })
-
-            cae.save()
-            train_in2, test_in2 = cae.encode(train_image), cae.encode(test_image)
-        
-        discriminator,_,_ = grid_search(curry(nn_task, PUDiscriminator, directory_sd,
+        train_in2, test_in2 = sae.get_features(train_image), sae.get_features(test_image)
+        discriminator,_,_ = grid_search(curry(nn_task, default_networks['PUDiscriminator'], sae.local("_sd3/"),
                                               train_in2, train_out, test_in2, test_out,),
                                         default_parameters,
                                         {
@@ -157,23 +112,118 @@ if __name__ == '__main__':
                                             'lr'         :[0.0001],
                                         })
         discriminator.save()
-        
-    else:
-        if 'conv' not in aetype:
-            cae           = default_networks['SimpleCAE'    ](sae.local("_cae")).load()
-        discriminator = default_networks['PUDiscriminator'](directory_sd).load()
+    if method is "cae":
+        # decode into image, learn a separate cae and learn from it
+        train_image, test_image = sae.decode_binary(train_in), sae.decode_binary(test_in)
+        cae,_,_ = grid_search(curry(nn_task, default_networks['SimpleCAE'],
+                                    sae.local("_cae"),
+                                    train_image, train_image, test_image, test_image),
+                              default_parameters,
+                              {
+                                  'num_layers' :[2],
+                                  'layer'      :[500],
+                                  'clayer'     :[16],
+                                  'dropout'    :[0.4],
+                                  'batch_size' :[4000],
+                                  'full_epoch' :[1000],
+                                  'activation' :['relu'],
+                                  'epoch'      :[30],
+                                  'lr'         :[0.001],
+                              })
+        cae.save()
+        train_in2, test_in2 = cae.encode(train_image), cae.encode(test_image)
+        discriminator,_,_ = grid_search(curry(nn_task, default_networks['PUDiscriminator'], sae.local("_sd3/"),
+                                              train_in2, train_out, test_in2, test_out,),
+                                        default_parameters,
+                                        {
+                                            'num_layers' :[1],
+                                            'layer'      :[50],
+                                            'clayer'     :[16],
+                                            'dropout'    :[0.8],
+                                            'batch_size' :[1000],
+                                            'full_epoch' :[1000],
+                                            'activation' :['relu'],
+                                            'epoch'      :[3000],
+                                            'lr'         :[0.0001],
+                                        })
+        discriminator.save()
+    if method is "direct":
+        # learn directly from the latent encoding
+        discriminator,_,_ = grid_search(curry(nn_task, default_networks['PUDiscriminator'], directory_sd,
+                                              train_in, train_out, test_in, test_out,),
+                                        default_parameters,
+                                        {
+                                            'layer'      :[300],# [400,4000],
+                                            'dropout'    :[0.1], #[0.1,0.4],
+                                            'num_layers' :[2],
+                                            'batch_size' :[1000],
+                                            'full_epoch' :[1000],
+                                            'activation' :['tanh'],
+                                            # quick eval
+                                            'epoch'      :[200],
+                                            'lr'         :[0.0001],
+                                        })
+        discriminator.save()
+    if method is "image":
+        # learn directly from the image
+        train_image, test_image = sae.decode_binary(train_in), sae.decode_binary(test_in)
+        discriminator,_,_ = grid_search(curry(nn_task, default_networks['PUDiscriminator'], directory_sd,
+                                              train_image, train_out, test_image, test_out,),
+                                        default_parameters,
+                                        {
+                                            'layer'      :[300],# [400,4000],
+                                            'dropout'    :[0.1], #[0.1,0.4],
+                                            'num_layers' :[2],
+                                            'batch_size' :[1000],
+                                            'full_epoch' :[1000],
+                                            'activation' :['tanh'],
+                                            # quick eval
+                                            'epoch'      :[200],
+                                            'lr'         :[0.0001],
+                                        })
+        discriminator.save()
 
-    # test if the learned action is correct
+def load(method):
+    global cae, discriminator
+    if method is "feature":
+        discriminator = default_networks['PUDiscriminator'](sae.local("_sd3/")).load()
+    if method is "cae":
+        cae = default_networks['SimpleCAE'    ](sae.local("_cae")).load()
+        discriminator = default_networks['PUDiscriminator'](sae.local("_sd3/")).load()
+    if method is "direct":
+        discriminator = default_networks['PUDiscriminator'](sae.local("_sd3/")).load()
+    if method is "image":
+        discriminator = default_networks['PUDiscriminator'](sae.local("_sd3/")).load()
 
-    states_valid = np.loadtxt("{}/all_states.csv".format(directory),dtype=np.int8)
+def load2(method):
+    global combined
+    if method is "feature":
+        def d (states):
+            return combined_discriminate2(states,sae,discriminator,batch_size=1000).round()
+        combined = d
+    if method is "cae":
+        def d (states):
+            return combined_discriminate(states,sae,cae,discriminator,batch_size=1000).round()
+        combined = d
+    if method is "direct":
+        def d (states):
+            return discriminator.discriminate(states,batch_size=1000).round()
+        combined = d
+    if method is "image":
+        def d (states):
+            images = sae.decode_binary(data,batch_size=1000)
+            return discriminator.discriminate(images,batch_size=1000).round()
+        combined = d
+
+def test(method):
+    states_valid = np.loadtxt(sae.local("all_states.csv"),dtype=np.int8)
     print("valid",states_valid.shape)
 
     from latplan.util.plot import plot_grid
 
-    if 'conv' in aetype:
-        type1_d = combined_discriminate2(states_valid,sae,discriminator,batch_size=1000).round()
-    else:
-        type1_d = combined_discriminate(states_valid,sae,cae,discriminator,batch_size=1000).round()
+    ################################################################
+    # type 1 error
+    type1_d = combined(states_valid)
     type1_error = np.sum(1- type1_d)
     print("type1 error:",type1_error,"/",len(states_valid),
           "Error ratio:", type1_error/len(states_valid) * 100, "%")
@@ -185,58 +235,58 @@ if __name__ == '__main__':
         plot_grid(type1_error_images,
                   w=20,
                   path=discriminator.local("type1_error.png"))
-    
+
+    ################################################################
+    # type 2 error
     inflation = 1
     _,_,_,_, _, states_invalid = prepare(states_valid,sae)
 
-    if 'check' in mode:
-        p = latplan.util.puzzle_module(directory)
-        # p.validate_states(sae.decode_binary(states_valid))
-        is_invalid = p.validate_states(sae.decode_binary(states_invalid))
-        states_invalid = states_invalid[np.logical_not(is_invalid)]
+    p = latplan.util.puzzle_module(sae.path)
+    is_valid = p.validate_states(sae.decode_binary(states_invalid))
+    states_invalid = states_invalid[np.logical_not(is_valid)]
 
+    if len(states_invalid) == 0:
+        print("We observed ZERO invalid states.")
+    else:
         plot_grid(sae.decode_binary(states_invalid)[:120],
-                  w=20,
-                  path=discriminator.local("surely_invalid_states.png"))
-    
-    if 'conv' in aetype:
-        type2_d = combined_discriminate2(states_invalid,sae,discriminator,batch_size=1000).round()
+              w=20,
+              path=discriminator.local("surely_invalid_states.png"))
+        type2_d = combined(states_invalid)
+        type2_error = np.sum(type2_d)
+        print("type2 error:",type2_error,"/",len(states_invalid),
+              "Error ratio:", type2_error/len(states_invalid) * 100, "%")
+
+        type2_error_images = sae.decode_binary(states_invalid[np.where(type2_d > 0.9)[0]])[:120]
+        if len(type2_error_images) == 0:
+            print("We observed ZERO type2-error! Hooray!")
+        else:
+            plot_grid(type2_error_images,
+                      w=20,
+                      path=discriminator.local("type2_error.png"))
+
+def main(directory, mode="test", method='feature'):
+    from latplan.util import get_ae_type
+    global sae
+    sae = default_networks[get_ae_type(directory)](directory).load()
+    import subprocess
+    subprocess.call(["mkdir","-p", sae.local("_sd3/")])
+
+    if 'learn' in mode:
+        learn(method)
     else:
-        type2_d = combined_discriminate(states_invalid,sae,cae,discriminator,batch_size=1000).round()
-    type2_error = np.sum(type2_d)
-    print("type2 error:",type2_error,"/",len(states_invalid),
-          "Error ratio:", type2_error/len(states_invalid) * 100, "%")
+        load(method)
 
-    type2_error_images = sae.decode_binary(states_invalid[np.where(type2_d > 0.9)[0]])[:120]
-    if len(type2_error_images) == 0:
-        print("We observed ZERO type2-error! Hooray!")
-    else:
-        plot_grid(type2_error_images,
-                  w=20,
-                  path=discriminator.local("type2_error.png"))
-    
-"""
+    load2(method)
 
-This is a wrong attempt; the better the SAE compression, the less the "invalid" states.
+    if 'test' in mode:
+        test(method)
 
-* Summary:
-Input: a subset of valid states and random bitstrings (invalid states)
-Output: a function that returns 0/1 for a state
-
-* Training:
-
-
-* Network:
-
-
-* Evaluation:
-
-Dataset and network: mnist_puzzle33_fc2
-
-type-1 error for the entire valid states (967680 states):
-MAE: 0.09277774112190677
-
-type-2 error for the invalid states:
-MAE: 0.03727853568254886
-
-"""
+if __name__ == '__main__':
+    import sys
+    try:
+        print(sys.argv)
+        main(*sys.argv[1:])
+    except:
+        import traceback
+        print(traceback.format_exc())
+        sys.exit("{} [directory] [mode=test] [method=feature]".format(sys.argv[0]))
