7fb8949a04013aef55f7ba647f87feba7f4c2d84
Masataro Asai <guicho2.71828@gmail.com>
2017-08-31 11:29:10 +0900

    generator do not use full memory

--- problem-instances/generate.py
+++ problem-instances/generate.py
@@ -6,6 +6,27 @@ import os.path
 import sys
 sys.path.append('../../')
 
+import matplotlib
+matplotlib.use('Agg')
+import tensorflow as tf
+import keras.backend as K
+tf.logging.set_verbosity(tf.logging.ERROR)
+# K.set_floatx('float16')
+print("Default float: {}".format(K.floatx()))
+
+def load_session():
+    K.set_session(
+        tf.Session(
+            config=tf.ConfigProto(
+                allow_soft_placement=True,
+                gpu_options =
+                tf.GPUOptions(
+                    per_process_gpu_memory_fraction=1.0,
+                    allow_growth=True,))))
+
+load_session()
+
+
 steps     = 5
 instances = 100
 noise     = None
