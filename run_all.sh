c0b78f2469be723619491db6146f8688e877ebe7
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 08:53:26 +0900

    run_all 180 only unsolved

--- run_all.sh
+++ run_all.sh
@@ -4,42 +4,42 @@ ulimit -v 16000000000
 
 trap exit SIGINT
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_mnist.csv  \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.puzzle_mnist.csv  \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: puzzle_mnist_3_3_36_20000_conv \
          ::: problem-instances/*/latplan.puzzles.puzzle_mnist/* \
          ::: Astar
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_mandrill.csv   \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.puzzle_mandrill.csv   \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: puzzle_mandrill_3_3_36_20000_conv \
          ::: problem-instances/*/latplan.puzzles.puzzle_mandrill/* \
          ::: Astar
 
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_spider.csv   \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.puzzle_spider.csv   \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: puzzle_spider_3_3_36_20000_conv \
          ::: problem-instances/*/latplan.puzzles.puzzle_spider/* \
          ::: Astar
 
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.lightsout_digital.csv   \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.lightsout_digital.csv   \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: lightsout_digital_4_36_20000_conv \
          ::: problem-instances/*/latplan.puzzles.lightsout_digital/* \
          ::: Astar
 
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.lightsout_twisted.csv   \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.lightsout_twisted.csv   \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: lightsout_twisted_4_36_20000_conv \
          ::: problem-instances/*/latplan.puzzles.lightsout_twisted/* \
          ::: Astar
 
 
-parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.hanoi.csv   \
-         "./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
+parallel --eta --timeout 180 --joblog problem-instances/latplan.puzzles.hanoi.csv   \
+         "test -e {2}/*.valid || ./trivial-planner.py samples/{1} {2} {3} > {2}/{1}_{3}.log" \
          ::: hanoi_4_3_36_60_conv hanoi_4_3_36_81_conv \
          ::: problem-instances/*/latplan.puzzles.hanoi/* \
          ::: Astar
