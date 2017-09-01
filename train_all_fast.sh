40861f70e539eef8e622e9512bac75efecf02d02
Masataro Asai <guicho2.71828@gmail.com>
2017-09-01 19:03:00 +0900

    train_all shows test results

--- train_all_fast.sh
+++ train_all_fast.sh
@@ -7,38 +7,38 @@ trap exit SIGINT
 ulimit -v 16000000000
 
 # ./strips.py conv puzzle learn_plot_dump mandrill 3 3 36 20000
-# ./state_discriminator3.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn &
-# ./action_autoencoder.py   samples/puzzle_mandrill_3_3_36_20000_conv/ learn &
+# ./state_discriminator3.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test &
+# ./action_autoencoder.py   samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test &
 # wait
-# ./action_discriminator.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn
+# ./action_discriminator.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test
 # 
 # ./strips.py conv puzzle learn_plot_dump mnist 3 3 36 20000
-# ./state_discriminator3.py samples/puzzle_mnist_3_3_36_20000_conv/ learn &
-# ./action_autoencoder.py   samples/puzzle_mnist_3_3_36_20000_conv/ learn &
+# ./state_discriminator3.py samples/puzzle_mnist_3_3_36_20000_conv/ learn_test &
+# ./action_autoencoder.py   samples/puzzle_mnist_3_3_36_20000_conv/ learn_test &
 # wait
-# ./action_discriminator.py samples/puzzle_mnist_3_3_36_20000_conv/ learn
+# ./action_discriminator.py samples/puzzle_mnist_3_3_36_20000_conv/ learn_test
 
 # ./strips.py conv puzzle learn_plot_dump spider 3 3 36 20000
-# ./state_discriminator3.py samples/puzzle_spider_3_3_36_20000_conv/ learn &
-# ./action_autoencoder.py   samples/puzzle_spider_3_3_36_20000_conv/ learn &
+# ./state_discriminator3.py samples/puzzle_spider_3_3_36_20000_conv/ learn_test &
+# ./action_autoencoder.py   samples/puzzle_spider_3_3_36_20000_conv/ learn_test &
 # wait
-# ./action_discriminator.py samples/puzzle_spider_3_3_36_20000_conv/ learn
+# ./action_discriminator.py samples/puzzle_spider_3_3_36_20000_conv/ learn_test
 
 ./strips.py conv lightsout learn_plot_dump digital 4 36 20000
-./state_discriminator3.py samples/lightsout_digital_4_36_20000_conv/ learn &
-./action_autoencoder.py   samples/lightsout_digital_4_36_20000_conv/ learn &
+./state_discriminator3.py samples/lightsout_digital_4_36_20000_conv/ learn_test &
+./action_autoencoder.py   samples/lightsout_digital_4_36_20000_conv/ learn_test &
 wait
-./action_discriminator.py samples/lightsout_digital_4_36_20000_conv/ learn
+./action_discriminator.py samples/lightsout_digital_4_36_20000_conv/ learn_test
 
 ./strips.py conv lightsout learn_plot_dump twisted 4 36 20000
-./state_discriminator3.py samples/lightsout_twisted_4_36_20000_conv/ learn &
-./action_autoencoder.py   samples/lightsout_twisted_4_36_20000_conv/ learn &
+./state_discriminator3.py samples/lightsout_twisted_4_36_20000_conv/ learn_test &
+./action_autoencoder.py   samples/lightsout_twisted_4_36_20000_conv/ learn_test &
 wait
-./action_discriminator.py samples/lightsout_twisted_4_36_20000_conv/ learn
+./action_discriminator.py samples/lightsout_twisted_4_36_20000_conv/ learn_test
 
 ./strips.py conv hanoi learn_plot_dump 4 3 36 60
-./state_discriminator3.py samples/hanoi_4_3_36_60_conv/ learn &
-./action_autoencoder.py   samples/hanoi_4_3_36_60_conv/ learn &
+./state_discriminator3.py samples/hanoi_4_3_36_60_conv/ learn_test &
+./action_autoencoder.py   samples/hanoi_4_3_36_60_conv/ learn_test &
 wait
-./action_discriminator.py samples/hanoi_4_3_36_60_conv/ learn
+./action_discriminator.py samples/hanoi_4_3_36_60_conv/ learn_test
 
