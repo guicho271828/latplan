f2e6b112c423b9133260c20876e4d4d6eb3d0dd9
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 17:38:23 +0900

    saltpepper noise

--- latplan/util/noise.py
+++ latplan/util/noise.py
@@ -10,4 +10,11 @@ def salt(a,p=0.06):
 def pepper(a,p=0.06):
     return np.clip(a - np.clip(np.sign(p - np.random.uniform(0,1,a.shape)), 0, 1), 0, 1)
 
+def saltpepper(a,p=0.06):
+    def sym(x):
+        return x*2-1
+    def zeroone(x):
+        return (x+1)/2
+    return zeroone(sym(a) * sym(np.clip(np.sign(np.random.uniform(0,1,a.shape) - p), 0, 1)))
+
 
