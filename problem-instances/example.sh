55ac9c1c407978ffff0317289403d5d16cd90e60
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 17:40:10 +0900

    problem generatotakes noise parameter arguments

--- problem-instances/example.sh
+++ problem-instances/example.sh
@@ -13,26 +13,26 @@ parallel --files <<EOF
 ./generate.py 7 100 lightsout twisted 4
 ./generate.py 6 100 hanoi 4 3
 
-./generate.py 7 100 noise gaussian puzzle mandrill 3 3
-./generate.py 7 100 noise gaussian puzzle spider   3 3
-./generate.py 7 100 noise gaussian puzzle mnist    3 3
-./generate.py 7 100 noise gaussian lightsout digital 4
-./generate.py 7 100 noise gaussian lightsout twisted 4
-./generate.py 6 100 noise gaussian hanoi 4 3
+./generate.py 7 100 noise gaussian 0.3 puzzle mandrill 3 3
+./generate.py 7 100 noise gaussian 0.3 puzzle spider   3 3
+./generate.py 7 100 noise gaussian 0.3 puzzle mnist    3 3
+./generate.py 7 100 noise gaussian 0.3 lightsout digital 4
+./generate.py 7 100 noise gaussian 0.3 lightsout twisted 4
+./generate.py 6 100 noise gaussian 0.3 hanoi 4 3
 
-./generate.py 7 100 noise salt puzzle mandrill 3 3
-./generate.py 7 100 noise salt puzzle spider   3 3
-./generate.py 7 100 noise salt puzzle mnist    3 3
-./generate.py 7 100 noise salt lightsout digital 4
-./generate.py 7 100 noise salt lightsout twisted 4
-./generate.py 6 100 noise salt hanoi 4 3
+./generate.py 7 100 noise salt 0.06 puzzle mandrill 3 3
+./generate.py 7 100 noise salt 0.06 puzzle spider   3 3
+./generate.py 7 100 noise salt 0.06 puzzle mnist    3 3
+./generate.py 7 100 noise salt 0.06 lightsout digital 4
+./generate.py 7 100 noise salt 0.06 lightsout twisted 4
+./generate.py 6 100 noise salt 0.06 hanoi 4 3
 
-./generate.py 7 100 noise pepper puzzle mandrill 3 3
-./generate.py 7 100 noise pepper puzzle spider   3 3
-./generate.py 7 100 noise pepper puzzle mnist    3 3
-./generate.py 7 100 noise pepper lightsout digital 4
-./generate.py 7 100 noise pepper lightsout twisted 4
-./generate.py 6 100 noise pepper hanoi 4 3
+./generate.py 7 100 noise pepper 0.06 puzzle mandrill 3 3
+./generate.py 7 100 noise pepper 0.06 puzzle spider   3 3
+./generate.py 7 100 noise pepper 0.06 puzzle mnist    3 3
+./generate.py 7 100 noise pepper 0.06 lightsout digital 4
+./generate.py 7 100 noise pepper 0.06 lightsout twisted 4
+./generate.py 6 100 noise pepper 0.06 hanoi 4 3
 
 EOF
 
