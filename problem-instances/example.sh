3980171b10d94c2a6d9d19fddf2cdde4b6229f10
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 17:41:08 +0900

    generate saltpepper only, remove salt and pepper

--- problem-instances/example.sh
+++ problem-instances/example.sh
@@ -20,19 +20,12 @@ parallel --files <<EOF
 ./generate.py 7 100 noise gaussian 0.3 lightsout twisted 4
 ./generate.py 6 100 noise gaussian 0.3 hanoi 4 3
 
-./generate.py 7 100 noise salt 0.06 puzzle mandrill 3 3
-./generate.py 7 100 noise salt 0.06 puzzle spider   3 3
-./generate.py 7 100 noise salt 0.06 puzzle mnist    3 3
-./generate.py 7 100 noise salt 0.06 lightsout digital 4
-./generate.py 7 100 noise salt 0.06 lightsout twisted 4
-./generate.py 6 100 noise salt 0.06 hanoi 4 3
-
-./generate.py 7 100 noise pepper 0.06 puzzle mandrill 3 3
-./generate.py 7 100 noise pepper 0.06 puzzle spider   3 3
-./generate.py 7 100 noise pepper 0.06 puzzle mnist    3 3
-./generate.py 7 100 noise pepper 0.06 lightsout digital 4
-./generate.py 7 100 noise pepper 0.06 lightsout twisted 4
-./generate.py 6 100 noise pepper 0.06 hanoi 4 3
+./generate.py 7 100 noise saltpepper 0.06 puzzle mandrill 3 3
+./generate.py 7 100 noise saltpepper 0.06 puzzle spider   3 3
+./generate.py 7 100 noise saltpepper 0.06 puzzle mnist    3 3
+./generate.py 7 100 noise saltpepper 0.06 lightsout digital 4
+./generate.py 7 100 noise saltpepper 0.06 lightsout twisted 4
+./generate.py 6 100 noise saltpepper 0.06 hanoi 4 3
 
 EOF
 
