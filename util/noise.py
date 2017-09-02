4f002cebbd49f8fabb65bd099eec82fd0886ff8b
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 17:38:09 +0900

    noise functions can take optional arguments

--- util/noise.py
+++ util/noise.py
@@ -1,12 +1,13 @@
 
 import numpy as np
 
-def gaussian(a):
-    return np.clip(np.random.normal(0,0.3,a.shape) + a, 0,1)
+def gaussian(a, sigma=0.3):
+    return np.clip(np.random.normal(0,sigma,a.shape) + a, 0,1)
 
-def salt(a):
-    return np.clip(np.clip(np.sign(0.06 - np.random.uniform(0,1,a.shape)), 0, 1) + a, 0, 1)
+def salt(a,p=0.06):
+    return np.clip(np.clip(np.sign(p - np.random.uniform(0,1,a.shape)), 0, 1) + a, 0, 1)
 
-def pepper(a):
-    return np.clip(a - np.clip(np.sign(0.06 - np.random.uniform(0,1,a.shape)), 0, 1), 0, 1)
+def pepper(a,p=0.06):
+    return np.clip(a - np.clip(np.sign(p - np.random.uniform(0,1,a.shape)), 0, 1), 0, 1)
+
 
