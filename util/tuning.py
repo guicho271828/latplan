88098f46ab32440cdae4e6d3225cd3e96bb75fd6
Masataro Asai <guicho2.71828@gmail.com>
2017-09-02 18:45:56 +0900

    refactor grid_search and complete greedy search

--- util/tuning.py
+++ util/tuning.py
@@ -28,12 +28,33 @@ def merge_hash(a, b):
         c[key] = value
     return c
 
+def _select(list):
+    import random
+    return list[random.randint(0,len(list)-1)]
+
+def _update_best(artifact, eval, local_parameters, results, best, report, report_best):
+    results.append((eval, local_parameters))
+    if report:
+        report(artifact)
+    print("Evaluation result for:\n{}\neval = {}".format(local_parameters,eval))
+    print("Current results:")
+    results.sort(key=lambda result: result[0])
+    [ print(r) for r in results]
+    if best['eval'] is None or eval < best['eval']:
+        print("Found a better parameter:\n{}\neval:{} old-best:{}".format(local_parameters,eval,best['eval']))
+        if report_best:
+            report_best(artifact)
+        del best['artifact']
+        best['params'] = local_parameters
+        best['eval'] = eval
+        best['artifact'] = artifact
+    else:
+        del artifact
+
 def grid_search(task, default_parameters, parameters,
                 report=None, report_best=None,
                 limit=float('inf')):
-    best_eval     = None
-    best_params   = None
-    best_artifact = None
+    best = {'eval'    :None, 'params'  :None, 'artifact':None}
     results       = []
     import itertools
     names  = [ k for k, _ in parameters.items()]
@@ -48,96 +69,71 @@ def grid_search(task, default_parameters, parameters,
             local_parameters = { k:v for k,v in zip(names,params) }
             print("{}/{} {}".format(i, len(all_params), local_parameters))
             artifact, eval = task(merge_hash(default_parameters,local_parameters))
-            results.append((eval, local_parameters))
-            if report:
-                report(artifact)
-            print("Evaluation result for:\n{}\neval = {}".format(local_parameters,eval))
-            print("Current results:")
-            results.sort(key=lambda result: result[0])
-            [ print(r) for r in results]
-            if best_eval is None or eval < best_eval:
-                print("Found a better parameter:\n{}\neval:{} old-best:{}".format(local_parameters,eval,best_eval))
-                if report_best:
-                    report_best(artifact)
-                del best_artifact
-                best_params = local_parameters
-                best_eval = eval
-                best_artifact = artifact
-            else:
-                del artifact
+            _update_best(artifact, eval, local_parameters, results, best, report, report_best)
     finally:
         from colors import bold
-        print(bold("*** Best parameter: ***\n{}\neval: {}".format(best_params,best_eval)))
+        print(bold("*** Best parameter: ***\n{}\neval: {}".format(best['params'],best['eval'])))
         print(results)
-    return best_artifact,best_params,best_eval
+    return best['artifact'],best['params'],best['eval']
 
-def best_first_search(task, default_parameters, parameters,
-                      initial_population=5,
-                      report=None,
-                      limit=float('inf'), runtime=float('inf')):
-    best = {'eval'    :float('inf'),
-            'params'  :None,
-            'artifact':None}
-    results       = []
-    results_hash  = {}
-    close_list    = {}
+def greedy_search(task, default_parameters, parameters,
+                  initial_population=3, limit=10,
+                  report=None, report_best=None,):
     import itertools
     names  = [ k for k, _ in parameters.items()]
     values = [ v for _, v in parameters.items()]
     all_params = list(itertools.product(*values))
     random.shuffle(all_params)
     [ print(r) for r in all_params]
-    def _iter(params):
-        local_parameters = { k:v for k,v in zip(names,params) }
-        print("{}/{} {}".format(i, len(all_params), local_parameters))
-        artifact, eval = task(merge_hash(default_parameters,local_parameters))
-        results.append((eval, local_parameters, params))
-        results.sort(key=lambda result: result[0])
-        results_hash[tuple(params)] = eval
-        close_list[tuple(params)] = False
-        print("Evaluation result for:\n{}\neval = {}".format(local_parameters,eval))
-        print("Current results:")
-        [ print(r) for r in results]
-        if eval < best_eval:
-            print("Found a better parameter:\n{}\neval:{} old-best:{}".format(local_parameters,eval,best_eval))
-            if report:
-                report(artifact)
-            del best_artifact
-            best['params'] = local_parameters
-            best['eval'] = eval
-            best['artifact'] = artifact
-        else:
-            del artifact
-    try:
-        for i,params in enumerate(all_params):
-            if i > initial_population:
-                break
-            _iter(params)
-        i = initial_population
+    best = {'eval'    :None, 'params'  :None, 'artifact':None}
+
+    import queue
+    open_list  = queue.PriorityQueue()
+    close_list = {}
+    results       = []          # for displaying
+
+    def random_configs():
+        import random
         while True:
-            neighbors = []
-            for _, _, params in results:
-                if close[params] is True:
-                    continue
-                close[params] = True
-                for dim, current in enumerate(params):
-                    for other_param in parameters[names[dim]]:
-                        if other_param != current:
-                            candidate = list(params).copy()
-                            candidate[dim] = other_param
-                            candidate = tuple(candidate)
-                            if candidate in close:
-                                if close[candidate] is False:
-                                    neighbors.append(candidate)
-                            else:
-                                neighbors.append(candidate)
-            random.shuffle(neighbors)
-            for neighbor in neighbors:
-                if i > limit:
-                    break
-                i += 1
-                _iter(neighbor)
+            yield { k : _select(v) for k, v in parameters.items() }
+
+    def neighbors(parent):
+        import random
+        results = []
+        for k in names:
+            for kv in parameters[k]:
+                if parent[k] is not kv:
+                    other = parent.copy()
+                    other[k] = kv
+                    results.append(other)
+        return results
+
+    def _key(local_parameters):
+        return tuple( local_parameters[k] for k in names )
+    
+    def _iter(local_parameters):
+        artifact, eval = task(merge_hash(default_parameters,local_parameters))
+        _update_best(artifact, eval, local_parameters, results, best, report, report_best)
+        # 
+        close_list[_key(local_parameters)] = eval # tuples are hashable
+        open_list.put((eval, local_parameters))
+        
+    try:
+        for i,local_parameters in zip(range(initial_population),random_configs()):
+            _iter(local_parameters)
+        for i in range(initial_population, limit):
+            if open_list.empty():
+                break
+            _, parent = open_list.get()
+            children = neighbors(parent)
+
+            open_children = []
+            for c in children:
+                if _key(c) not in close_list:
+                    open_children.append(c)
+            
+            _iter(_select(open_children))
     finally:
-        print("Best parameter:\n{}\neval: {}".format(best_params,best_eval))
+        print("Best parameter:\n{}\neval: {}".format(best['params'],best['eval']))
         print(results)
-    return best_artifact,best_params,best_eval
+    return best['artifact'],best['params'],best['eval']
