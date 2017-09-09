7f493296d75537286af8d303187f568458a7f5fd
Masataro Asai <guicho2.71828@gmail.com>
2017-09-10 00:25:15 +0900

    run_amaX_all: don't be fooled by similarly named files

--- run_ama2_all.sh
+++ run_ama2_all.sh
@@ -6,42 +6,42 @@ trap exit SIGINT
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_mnist.csv  \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/puzzle_mnist* \
-         ::: problem-instances/*/latplan.puzzles.puzzle_mnist/* \
+         ::: samples/puzzle_mnist*/ \
+         ::: problem-instances/*/latplan.puzzles.puzzle_mnist/*/ \
          ::: Astar
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_mandrill.csv   \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/puzzle_mandrill* \
-         ::: problem-instances/*/latplan.puzzles.puzzle_mandrill/* \
+         ::: samples/puzzle_mandrill*/ \
+         ::: problem-instances/*/latplan.puzzles.puzzle_mandrill/*/ \
          ::: Astar
 
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.puzzle_spider.csv   \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/puzzle_spider* \
-         ::: problem-instances/*/latplan.puzzles.puzzle_spider/* \
+         ::: samples/puzzle_spider*/ \
+         ::: problem-instances/*/latplan.puzzles.puzzle_spider/*/ \
          ::: Astar
 
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.lightsout_digital.csv   \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/lightsout_digital* \
-         ::: problem-instances/*/latplan.puzzles.lightsout_digital/* \
+         ::: samples/lightsout_digital*/ \
+         ::: problem-instances/*/latplan.puzzles.lightsout_digital/*/ \
          ::: Astar
 
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.lightsout_twisted.csv   \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/lightsout_twisted* \
-         ::: problem-instances/*/latplan.puzzles.lightsout_twisted/* \
+         ::: samples/lightsout_twisted*/ \
+         ::: problem-instances/*/latplan.puzzles.lightsout_twisted/*/ \
          ::: Astar
 
 
 parallel --eta --timeout 90 --joblog problem-instances/latplan.puzzles.hanoi.csv   \
          "./trivial-planner.py {1} {2} {3} > {2}/{1/}_{3}.log" \
-         ::: samples/hanoi* \
-         ::: problem-instances/*/latplan.puzzles.hanoi/* \
+         ::: samples/hanoi*/ \
+         ::: problem-instances/*/latplan.puzzles.hanoi/*/ \
          ::: Astar
 
 
