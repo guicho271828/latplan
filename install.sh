8f54f2318c6b67e95b3c4d6dfddc756e0f5c9838
Masataro Asai <guicho2.71828@gmail.com>
2017-10-18 18:13:07 +0900

    fix: do not overwrite, append

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ set -e
     cp keras-tf.json ~/.keras/keras.json
 }
 
-cat > ~/.profile <<EOF
+cat >> ~/.profile <<EOF
 
 # This is added by latplan install.sh script.
 export PYTHONPATH="$PWD:\$PYTHONPATH"
