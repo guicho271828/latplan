2cc31baf9a8c3859b9b68adfc52c4b66dba85572
Masataro Asai <guicho2.71828@gmail.com>
2017-11-10 15:40:35 +0900

    more fix install.sh

--- install.sh
+++ install.sh
@@ -10,37 +10,34 @@ set -e
 #     echo "This is not ubuntu! Modify install.sh by yourself" >&2
 # fi
 
-{
-    sudo apt install -y mercurial g++ cmake make python flex bison g++-multilib
-    git submodule update --init --recursive
-    cd downward ; ./build.py -j $(cat /proc/cpuinfo | grep -c processor) release64
-}
+sudo apt install -y mercurial g++ cmake make python flex bison g++-multilib
+git submodule update --init --recursive
+(
+    cd downward
+    ./build.py -j $(cat /proc/cpuinfo | grep -c processor) release64
+)
 
 
-{
-    # https://github.com/roswell/roswell/wiki/1.-Installation
-    sudo apt -y install git build-essential automake libcurl4-openssl-dev
-    git clone -b release https://github.com/roswell/roswell.git
-    (
-        cd roswell
-        sh bootstrap
-        ./configure
-        make
-        sudo make install
-        ros setup
-    )
-    make -j 1 -C lisp
-}
+# https://github.com/roswell/roswell/wiki/1.-Installation
+sudo apt -y install git build-essential automake libcurl4-openssl-dev
+git clone -b release https://github.com/roswell/roswell.git
+(
+    cd roswell
+    sh bootstrap
+    ./configure
+    make
+    sudo make install
+    ros setup
+)
+make -j 1 -C lisp
 
-{
-    sudo apt -y install python3-pip python3-pil parallel \
-         bash-completion byobu htop parallel mosh git
-    pip3 install --user \
-         tensorflow keras h5py matplotlib progressbar2 \
-         timeout_decorator ansicolors scipy scikit-image
-    mkdir -p ~/.keras
-    cp keras-tf.json ~/.keras/keras.json
-}
+sudo apt -y install python3-pip python3-pil parallel \
+     bash-completion byobu htop parallel mosh git
+pip3 install --user \
+     tensorflow keras h5py matplotlib progressbar2 \
+     timeout_decorator ansicolors scipy scikit-image
+mkdir -p ~/.keras
+cp keras-tf.json ~/.keras/keras.json
 
 cat >> ~/.profile <<EOF
 
@@ -48,6 +45,4 @@ cat >> ~/.profile <<EOF
 export PYTHONPATH="$PWD:\$PYTHONPATH"
 
 EOF
-
-
 
