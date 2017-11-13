5f36b2ade718d3f89dcb5d92279d6f8423407547
Masataro Asai <guicho2.71828@gmail.com>
2017-11-13 19:04:41 +0900

    remove unnecessary sh files

--- do-everything.sh
+++ do-everything.sh
@@ -3,4 +3,4 @@
 (cd problem-instances; ./example.sh)
 ./train_all_fast.sh
 ./test_all.sh
-./run_all.sh
+
--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo  $(dirname $0)
-rsync -av \
-      -m \
-      --include '*/' --include '*.png' --include '*.log' --include '*.valid' --exclude '*' \
-      --delete \
-      $(dirname $0)/ ~/Dropbox/FukunagaLabShare/OngoingWorks/Asai/latent-planner/sync/
