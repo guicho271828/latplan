e9a74e863f535965adc0be1a9df81ffd2c2a165d
Masataro Asai <Masataro.Asai@ibm.com>
2018-04-30 19:42:12 +0900

    [config]trivial-planner uses config_cpu

---
+++ config_cpu.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('Agg')
+import tensorflow as tf
+import keras.backend as K
+tf.logging.set_verbosity(tf.logging.ERROR)
+# K.set_floatx('float16')
+print("Default float: {}".format(K.floatx()))
+
+def load_session():
+    K.set_session(
+        tf.Session(
+            config=tf.ConfigProto(
+                allow_soft_placement=True,
+                intra_op_parallelism_threads=1,
+                inter_op_parallelism_threads=1,
+                device_count = {'CPU': 1, 'GPU': 0},
+                gpu_options =
+                tf.GPUOptions(
+                    per_process_gpu_memory_fraction=1.0,
+                    allow_growth=True,))))
+
+load_session()
+clear_session = K.clear_session
+
+def reload_session():
+    clear_session()
+    load_session()
