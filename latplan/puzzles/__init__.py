f8409b2c1128d41c25ba09aafda8d7d71dbdef6d
Masataro Asai <Masataro.Asai@ibm.com>
2018-07-08 01:17:26 +0900

    shuffle_object

--- latplan/puzzles/__init__.py
+++ latplan/puzzles/__init__.py
@@ -13,3 +13,12 @@ from . import puzzle_mnist
 from . import puzzle_wrong
 from . import puzzle_spider
 from . import split_image
+
+def shuffle_objects(objects):
+    import numpy as np
+    import numpy.random
+    tmp = np.copy(objects)
+    tmp = np.transpose(tmp,(1,0,2))
+    np.random.shuffle(tmp)
+    tmp = np.transpose(tmp,(1,0,2))
+    return tmp
