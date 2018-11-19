0747efc048955edb76e8255a9c67387baa294333
Masataro Asai Masataro.Asai@ibm.com <guicho2.71828@gmail.com>
2018-11-19 17:57:16 +0900

    [helper] ensure the output files are deleted when it is terminated

--- helper/sasp.sh
+++ helper/sasp.sh
@@ -2,5 +2,7 @@
 
 # dirty hack because python subprocess sucks
 
+trap "rm $2" ERR
+
 echo $0 $@
 [ -s $2 ] || ( gunzip < $1 | $(dirname $0)/../downward/builds/release64/bin/preprocess | gzip > $2 )
