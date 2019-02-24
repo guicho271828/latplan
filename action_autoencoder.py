298be0a241c40a68da55fb95850ab1a6c9461c50
Masataro Asai Masataro.Asai@ibm.com <guicho2.71828@gmail.com>
2019-02-24 14:14:13 +0900

    [AAE] action_autoencoder: unindent, remove __main__

--- action_autoencoder.py
+++ action_autoencoder.py
@@ -28,127 +28,126 @@ default_parameters = {
 
 num_actions = 128
 
-if __name__ == '__main__':
-    import numpy.random as random
+import numpy.random as random
 
-    import sys
-    if len(sys.argv) == 1:
-        sys.exit("{} [directory]".format(sys.argv[0]))
+import sys
+if len(sys.argv) == 1:
+    sys.exit("{} [directory]".format(sys.argv[0]))
 
-    directory = sys.argv[1]
-    directory_aae = "{}/_aae/".format(directory)
-    mode = sys.argv[2]
+directory = sys.argv[1]
+directory_aae = "{}/_aae/".format(directory)
+mode = sys.argv[2]
+
+from latplan.util import get_ae_type
+ae = latplan.model.get(get_ae_type(directory))(directory).load()
+
+if "hanoi" in ae.path:
+    data = np.loadtxt(ae.local("all_actions.csv"),dtype=np.int8)
+else:
+    data = np.loadtxt(ae.local("actions.csv"),dtype=np.int8)
+
+parameters = {
+    'N'          :[1],
+    'M'          :[num_actions],
+    'layer'      :[400],# 200,300,400,700,1000
+    'encoder_layers' : [2], # 0,2,3
+    'decoder_layers' : [2], # 0,1,3
+    'dropout'    :[0.4], #[0.1,0.4],
+    # 'dropout_z'  :[False],
+    'batch_size' :[2000],
+    'full_epoch' :[1000],
+    'epoch'      :[1000],
+    'encoder_activation' :['relu'], # 'tanh'
+    'decoder_activation' :['relu'], # 'tanh',
+    # quick eval
+    'lr'         :[0.001],
+}
+print(data.shape)
+try:
+    if 'learn' in mode:
+        raise Exception('learn')
+    aae = ActionAE(directory_aae).load()
+except:
+    aae,_,_ = grid_search(curry(nn_task, ActionAE, directory_aae,
+                                data[:int(len(data)*0.9)], data[:int(len(data)*0.9)],
+                                data[int(len(data)*0.9):], data[int(len(data)*0.9):],),
+                          default_parameters,
+                          parameters)
+    aae.save()
+
+N = data.shape[1]//2
+
+actions = aae.encode_action(data, batch_size=1000).round()
+histogram = np.squeeze(actions.sum(axis=0,dtype=int))
+print(histogram)
+print(np.count_nonzero(histogram > 0))
+effective_labels = np.count_nonzero(histogram)
+all_labels = np.zeros((effective_labels, actions.shape[1], actions.shape[2]), dtype=int)
+for i, pos in enumerate(np.where(histogram > 0)[0]):
+    all_labels[i][0][pos] = 1
+
+if 'plot' in mode:
+    aae.plot(data[:8], "aae_train.png")
+    aae.plot(data[int(len(data)*0.9):int(len(data)*0.9)+8], "aae_test.png")
     
-    from latplan.util import get_ae_type
-    ae = latplan.model.get(get_ae_type(directory))(directory).load()
-
-    if "hanoi" in ae.path:
-        data = np.loadtxt(ae.local("all_actions.csv"),dtype=np.int8)
-    else:
-        data = np.loadtxt(ae.local("actions.csv"),dtype=np.int8)
     
-    parameters = {
-        'N'          :[1],
-        'M'          :[num_actions],
-        'layer'      :[400],# 200,300,400,700,1000
-        'encoder_layers' : [2], # 0,2,3
-        'decoder_layers' : [2], # 0,1,3
-        'dropout'    :[0.4], #[0.1,0.4],
-        # 'dropout_z'  :[False],
-        'batch_size' :[2000],
-        'full_epoch' :[1000],
-        'epoch'      :[1000],
-        'encoder_activation' :['relu'], # 'tanh'
-        'decoder_activation' :['relu'], # 'tanh',
-        # quick eval
-        'lr'         :[0.001],
-    }
-    print(data.shape)
+    aae.plot(data[:8], "aae_train_decoded.png", ae=ae)
+    aae.plot(data[int(len(data)*0.9):int(len(data)*0.9)+8], "aae_test_decoded.png", ae=ae)
+    
+    transitions = aae.decode([np.repeat(data[:1,:N], len(all_labels), axis=0), all_labels])
+    aae.plot(transitions, "aae_all_actions_for_a_state.png", ae=ae)
+    
+    from latplan.util.timer import Timer
+    # with Timer("loading csv..."):
+    #     all_actions = np.loadtxt("{}/all_actions.csv".format(directory),dtype=np.int8)
+    # transitions = aae.decode([np.repeat(all_actions[:1,:N], len(all_labels), axis=0), all_labels])
+    suc = transitions[:,N:]
+    from latplan.util.plot import plot_grid, squarify
+    plot_grid([x for x in ae.decode(suc)], w=8, path=aae.local("aae_all_actions_for_a_state_8x16.png"), verbose=True)
+    plot_grid([x for x in ae.decode(suc)], w=16, path=aae.local("aae_all_actions_for_a_state_16x8.png"), verbose=True)
+    plot_grid(ae.decode(data[:1,:N]), w=1, path=aae.local("aae_all_actions_for_a_state_state.png"), verbose=True)
+    
+
+if 'test' in mode:
+    from latplan.util.timer import Timer
+    with Timer("loading csv..."):
+        all_actions = np.loadtxt("{}/all_actions.csv".format(directory),dtype=np.int8)
+
+    with Timer("shuffling"):
+        random.shuffle(all_actions)
+    all_actions = all_actions[:10000]
+
+    count = 0
     try:
-        if 'learn' in mode:
-            raise Exception('learn')
-        aae = ActionAE(directory_aae).load()
-    except:
-        aae,_,_ = grid_search(curry(nn_task, ActionAE, directory_aae,
-                                    data[:int(len(data)*0.9)], data[:int(len(data)*0.9)],
-                                    data[int(len(data)*0.9):], data[int(len(data)*0.9):],),
-                              default_parameters,
-                              parameters)
-        aae.save()
+        pre_states = all_actions[:,:N]
+        suc_states = all_actions[:,N:]
+        pre_images = ae.decode(pre_states,batch_size=1000)
+        suc_images = ae.decode(suc_states,batch_size=1000)
 
-    N = data.shape[1]//2
-    
-    actions = aae.encode_action(data, batch_size=1000).round()
-    histogram = np.squeeze(actions.sum(axis=0,dtype=int))
-    print(histogram)
-    print(np.count_nonzero(histogram > 0))
-    effective_labels = np.count_nonzero(histogram)
-    all_labels = np.zeros((effective_labels, actions.shape[1], actions.shape[2]), dtype=int)
-    for i, pos in enumerate(np.where(histogram > 0)[0]):
-        all_labels[i][0][pos] = 1
-    
-    if 'plot' in mode:
-        aae.plot(data[:8], "aae_train.png")
-        aae.plot(data[int(len(data)*0.9):int(len(data)*0.9)+8], "aae_test.png")
-        
-        
-        aae.plot(data[:8], "aae_train_decoded.png", ae=ae)
-        aae.plot(data[int(len(data)*0.9):int(len(data)*0.9)+8], "aae_test_decoded.png", ae=ae)
-        
-        transitions = aae.decode([np.repeat(data[:1,:N], len(all_labels), axis=0), all_labels])
-        aae.plot(transitions, "aae_all_actions_for_a_state.png", ae=ae)
-        
-        from latplan.util.timer import Timer
-        # with Timer("loading csv..."):
-        #     all_actions = np.loadtxt("{}/all_actions.csv".format(directory),dtype=np.int8)
-        # transitions = aae.decode([np.repeat(all_actions[:1,:N], len(all_labels), axis=0), all_labels])
-        suc = transitions[:,N:]
-        from latplan.util.plot import plot_grid, squarify
-        plot_grid([x for x in ae.decode(suc)], w=8, path=aae.local("aae_all_actions_for_a_state_8x16.png"), verbose=True)
-        plot_grid([x for x in ae.decode(suc)], w=16, path=aae.local("aae_all_actions_for_a_state_16x8.png"), verbose=True)
-        plot_grid(ae.decode(data[:1,:N]), w=1, path=aae.local("aae_all_actions_for_a_state_state.png"), verbose=True)
-        
-    
-    if 'test' in mode:
-        from latplan.util.timer import Timer
-        with Timer("loading csv..."):
-            all_actions = np.loadtxt("{}/all_actions.csv".format(directory),dtype=np.int8)
+        for pre_state,suc_state,pre_image,suc_image in zip(pre_states,suc_states,pre_images,suc_images):
+            
+            generated_transitions = aae.decode([
+                np.repeat([pre_state],effective_labels,axis=0),
+                all_labels,
+            ],batch_size=1000)
+            generated_suc_states = generated_transitions[:,N:]
+            generated_suc_images = ae.decode(generated_suc_states,batch_size=1000)
 
-        with Timer("shuffling"):
-            random.shuffle(all_actions)
-        all_actions = all_actions[:10000]
+            from latplan.util import bce
+            errors = bce(generated_suc_images, np.repeat([suc_image],effective_labels,axis=0), axis=(1,2))
+            min_error = np.amin(errors)
+            if min_error < 0.01:
+                count += 1
+    finally:
+        import json
+        with open(aae.local("performance.json"),"w") as f:
+            json.dump({"count": count, "total":len(all_actions)}, f)
 
-        count = 0
-        try:
-            pre_states = all_actions[:,:N]
-            suc_states = all_actions[:,N:]
-            pre_images = ae.decode(pre_states,batch_size=1000)
-            suc_images = ae.decode(suc_states,batch_size=1000)
-
-            for pre_state,suc_state,pre_image,suc_image in zip(pre_states,suc_states,pre_images,suc_images):
-                
-                generated_transitions = aae.decode([
-                    np.repeat([pre_state],effective_labels,axis=0),
-                    all_labels,
-                ],batch_size=1000)
-                generated_suc_states = generated_transitions[:,N:]
-                generated_suc_images = ae.decode(generated_suc_states,batch_size=1000)
-
-                from latplan.util import bce
-                errors = bce(generated_suc_images, np.repeat([suc_image],effective_labels,axis=0), axis=(1,2))
-                min_error = np.amin(errors)
-                if min_error < 0.01:
-                    count += 1
-        finally:
-            import json
-            with open(aae.local("performance.json"),"w") as f:
-                json.dump({"count": count, "total":len(all_actions)}, f)
-    
-    if "dump" in mode:
-        # one-hot to id
-        actions_byid = (actions * np.arange(num_actions)).sum(axis=-1,dtype=int)
-        with open(ae.local("action_ids.csv"), 'wb') as f:
-            np.savetxt(f,actions_byid,"%d")
+if "dump" in mode:
+    # one-hot to id
+    actions_byid = (actions * np.arange(num_actions)).sum(axis=-1,dtype=int)
+    with open(ae.local("action_ids.csv"), 'wb') as f:
+        np.savetxt(f,actions_byid,"%d")
         
         
         
