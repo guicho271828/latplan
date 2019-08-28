cf8f3c384708f65f4760dd7cf131e14343820c28
Masataro Asai <guicho2.71828@gmail.com>
2019-08-28 14:41:50 -0400

    renamed planners: fd-planner -> ama1-planner, and trivial-planner -> ama2-planner

--- run_ama2_all.sh
+++ run_ama2_all.sh
@@ -11,36 +11,36 @@ dir=$(dirname $(dirname $(readlink -ef $0)))
 export PYTHONPATH=$dir:$PYTHONPATH
 export PYTHONUNBUFFERED=1
 
-parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
          ::: $(ls -d samples/puzzle*mnist* | grep -v 1000 | grep -v convn ) \
          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.puzzle_mnist/* \
          ::: Astar
 
-parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
          ::: $(ls -d samples/puzzle*mandrill* | grep -v 1000 | grep -v convn ) \
          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.puzzle_mandrill/* \
          ::: Astar
 
 
-parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
          ::: $(ls -d samples/puzzle*spider* | grep -v 1000 | grep -v convn ) \
          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.puzzle_spider/* \
          ::: Astar
 
 
-parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
          ::: $(ls -d samples/lightsout*digital* | grep -v 1000 | grep -v convn ) \
          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.lightsout_digital/* \
          ::: Astar
 
 
-parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
          ::: $(ls -d samples/lightsout*twisted* | grep -v 1000 | grep -v convn ) \
          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.lightsout_twisted/* \
          ::: Astar
 
 
-# parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './trivial-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
+# parallel   "[ -f {2}/{1/}_{3}_path_0.valid ] || $common './ama2-planner.py {1} {2} {3}  > {2}/{1/}_{3}.log'" \
 #          ::: samples/hanoi* \
 #          ::: noise-0.6-0.12-ama2/*/latplan.puzzles.hanoi/* \
 #          ::: Astar
