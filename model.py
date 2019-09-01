f074f301f532603010112e6e2f5b3cb9caa01bcd
Masataro Asai <guicho2.71828@gmail.com>
2019-09-01 13:31:06 -0400

    GS supports offset

--- model.py
+++ model.py
@@ -362,10 +362,12 @@ The latter two are used for verifying the performance of the AE.
                max_temperature=self.parameters['max_temperature'],
                min_temperature=self.parameters['min_temperature'],
                full_epoch=self.parameters['full_epoch'],
+               offset=0,
                argmax=self.parameters['argmax'],
                alpha=-1.):
             gs = GumbelSoftmax(
                 N,M,min_temperature,max_temperature,full_epoch,
+                offset=offset,
                 test_gumbel=not argmax,
                 test_softmax=not argmax,
                 # Entropy Regularization
