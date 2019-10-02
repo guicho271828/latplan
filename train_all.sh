c15f4150b7f805560a365a07cbf95d9b8d38f3c5
Masataro Asai <guicho2.71828@gmail.com>
2019-10-02 16:53:16 -0400

    scripts for ZSAE

--- train_all.sh
+++ train_all.sh
@@ -8,29 +8,31 @@ ulimit -v 16000000000
 
 dir=$(dirname $(dirname $(readlink -ef $0)))
 
-common="PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1"
+common="--dry-run --keep-order -j 1 PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1"
 
-$common ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE mandrill 3 3 36 20000
-$common ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE mnist 3 3 36 20000
-$common ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE spider 3 3 36 20000
-$common ./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE digital 4 36 20000
-$common ./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE twisted 4 36 20000
-$common ./strips.py learn_plot_dump hanoi     ConvolutionalGumbelAE 4 3 36 81
+# Note: ZSAE with alpha=0.0 is same as normal SAE.
+# Note: you can train NG-SAE (i.e. the "correct" VAE in the paper) by NGZeroSuppressConvolutionalGumbelAE
+#                                                                                                           N             examples  alpha
+parallel $common ./strips.py learn_plot_dump puzzle    ZeroSuppressConvolutionalGumbelAE mandrill 3 3 ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./strips.py learn_plot_dump puzzle    ZeroSuppressConvolutionalGumbelAE mnist 3 3    ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./strips.py learn_plot_dump puzzle    ZeroSuppressConvolutionalGumbelAE spider 3 3   ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./strips.py learn_plot_dump lightsout ZeroSuppressConvolutionalGumbelAE digital 4    ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./strips.py learn_plot_dump lightsout ZeroSuppressConvolutionalGumbelAE twisted 4    ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
 
-$common ./state_discriminator3.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
-$common ./state_discriminator3.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
-$common ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
-$common ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
-$common ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
+parallel $common ./state_discriminator3.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_digital_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./state_discriminator3.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_twisted_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./state_discriminator3.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mandrill_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./state_discriminator3.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mnist_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./state_discriminator3.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_spider_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
 
-$common ./action_autoencoder.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
-$common ./action_autoencoder.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
-$common ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
-$common ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
-$common ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
+parallel $common ./action_autoencoder.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_digital_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_autoencoder.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_twisted_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_autoencoder.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mandrill_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_autoencoder.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mnist_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_autoencoder.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_spider_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
 
-$common ./action_discriminator.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
-$common ./action_discriminator.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
-$common ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
-$common ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
-$common ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
+parallel $common ./action_discriminator.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_digital_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_discriminator.py samples/lightsout_ZeroSuppressConvolutionalGumbelAE_twisted_4_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_discriminator.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mandrill_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_discriminator.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_mnist_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
+parallel $common ./action_discriminator.py samples/puzzle_ZeroSuppressConvolutionalGumbelAE_spider_3_3_{1}_{2}_{3} learn_test ::: 36 64 100 ::: 10000 ::: 0.0 0.2 0.5 0.7
