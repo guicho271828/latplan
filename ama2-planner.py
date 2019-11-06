ac23bf975e5f64796daf14a5b60567fe912ca3a0
Masataro Asai <guicho2.71828@gmail.com>
2019-11-06 11:13:21 -0500

    ama2: moved planner utility code to util/planner.py

--- ama2-planner.py
+++ ama2-planner.py
@@ -9,6 +9,7 @@ from latplan.model import combined_sd
 from latplan.util import *
 from latplan.util.plot import *
 from latplan.util.np_distances import *
+from latplan.util.planner import *
 import os.path
 import keras.backend as K
 import tensorflow as tf
@@ -305,6 +306,10 @@ def blind(state,goal):
 def main(network_dir, problem_dir, searcher, first_solution=False):
     global sae, oae, ad, sd3, cae, available_actions
     
+    def search(path):
+        root, ext = os.path.splitext(path)
+        return "{}_{}{}".format(searcher, root, ext)
+
     p = latplan.util.puzzle_module(network_dir)
 
     sae = latplan.model.load(network_dir,allow_failure=True)
@@ -312,69 +317,11 @@ def main(network_dir, problem_dir, searcher, first_solution=False):
     ad  = latplan.model.load(sae.local("_ad/"),allow_failure=True)
     sd3 = latplan.model.load(sae.local("_sd3/"),allow_failure=True)
     cae = latplan.model.load(sae.local("_cae/"),allow_failure=True)
+    setup_planner_utils(sae, problem_dir, network_dir, "ama2")
 
     decide_pruning_method()
     
-
-    def problem(path):
-        return os.path.join(problem_dir,path)
-    def network(path):
-        root, ext = os.path.splitext(path)
-        return "{}_{}{}".format(ensure_directory(network_dir).split("/")[-2], root, ext)
-    def search(path):
-        root, ext = os.path.splitext(path)
-        return "{}_{}{}".format(searcher, root, ext)
-
-    import imageio
-    from latplan.puzzles.util import preprocess, normalize
-    # is already enhanced, equalized
-    init_image = normalize(imageio.imread(problem("init.png")))
-    goal_image = normalize(imageio.imread(problem("goal.png")))
-    print("init:",init_image.min(),init_image.max(),)
-    print("goal:",goal_image.min(),goal_image.max(),)
-    init = sae.encode(np.expand_dims(init_image,0))[0].round().astype(int)
-    goal = sae.encode(np.expand_dims(goal_image,0))[0].round().astype(int)
-    print(init)
-    print(goal)
-    rec = sae.decode(np.array([init,goal]))
-    init_rec, goal_rec = rec
-    print("init (reconstruction):",init_rec.min(),init_rec.max(),)
-    print("goal (reconstruction):",goal_rec.min(),goal_rec.max(),)
-
-    def r(i):
-        s = i.shape
-        return i.reshape((s[0]//2, 2, s[1]//2, 2)).mean(axis=(1,3))
-    
-    plot_grid([init_image,init_rec,init_image-init_rec,(init_image-init_rec).round(),
-               init_image.round(),init_rec.round(),init_image.round()-init_rec.round(),(init_image.round()-init_rec.round()).round(),
-               r(init_image),r(init_rec),r(init_image)-r(init_rec),(r(init_image)-r(init_rec)).round(),
-               # r(init_image).round(),r(init_rec).round(),r(init_image).round()-r(init_rec).round(),(r(init_image).round()-r(init_rec).round()).round(),
-               
-               goal_image,goal_rec,goal_image-goal_rec,(goal_image-goal_rec).round(),
-               goal_image.round(),goal_rec.round(),goal_image.round()-goal_rec.round(),(goal_image.round()-goal_rec.round()).round(),
-               r(goal_image),r(goal_rec),r(goal_image)-r(goal_rec),(r(goal_image)-r(goal_rec)).round(),
-               # r(goal_image).round(),r(goal_rec).round(),r(goal_image).round()-r(goal_rec).round(),(r(goal_image).round()-r(goal_rec).round()).round(),
-               ],
-              w=4,
-              path=problem(network("init_goal_reconstruction.png")),verbose=True)
-
-    import sys
-    print("init BCE:",bce(init_image,init_rec))
-    print("init MAE:",mae(init_image,init_rec))
-    print("init MSE:",mse(init_image,init_rec))
-    # if image_diff(init_image,init_rec) > image_threshold:
-    #     print("Initial state reconstruction failed!")
-    #     sys.exit(3)
-    print("goal BCE:",bce(goal_image,goal_rec))
-    print("goal MAE:",mae(goal_image,goal_rec))
-    print("goal MSE:",mse(goal_image,goal_rec))
-    # if image_diff(goal_image,goal_rec) > image_threshold:
-    #     print("Goal state reconstruction failed!")
-    #     sys.exit(4)
-    if not np.all(p.validate_states(rec)):
-        print("Init/Goal state reconstruction failed!")
-        # sys.exit(3)
-        print("But we continue anyways...")
+    init, goal = init_goal_misc(p)
     
     known_transisitons = np.loadtxt(sae.local("actions.csv"),dtype=np.int8)
     actions = oae.encode_action(known_transisitons, batch_size=1000).round()
