29c6ba8be673f4ca3723cc27b04bbf31a54c3638
Masataro Asai <guicho2.71828@gmail.com>
2019-11-06 16:24:00 -0500

    run_ama3_all: documentatin

--- run_ama3_all.sh
+++ run_ama3_all.sh
@@ -4,23 +4,6 @@ ulimit -v 16000000000
 
 trap exit SIGINT
 
-# Note: AMA3 requires huge memory and runtime for preprocessing.
-# For example:
-# puzzle instances require 4.5GB per process / 2 hours,
-# lightsout instances require 7.5GB per process / 4 hours on Xeon E5-2676 2.4 GHz.
-# Each SAS+ file may become over 1GB.
-
-# re: behavior --- The preprocessing results are precious. They are always
-# unique for each problem, irregardless of heuristics. However, due to the huge
-# memory requirement, it is inefficient to preprocess the same
-# problem independently.
-# 
-# Therefore, when a process is preprocessing an instance, other
-# instances solving the same instances are waited through a file lock.
-# 
-# Note that even when a ama3-planner process is waiting, it consumes nearly 700MB
-# for already loaded NN image.
-
 # Desired usage of this script is "./run_ama3_all.sh | parallel -j <number of processes>"
 # where the number should be adjusted for the resource capacity on your system.
 
