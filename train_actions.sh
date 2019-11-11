3645896e42249ca84624c139e8d0a8a39b97e20a
Masataro Asai <guicho2.71828@gmail.com>
2019-11-11 14:17:02 -0500

    table: init
    AAE: generate actions+ids, fake/invalid/valid/available_actions
    rf-accuracy: product of bitwise accuracy, all bitwise accuracy, precision, recall, f, specify the filename
    rf-accuracy: parallelize
    rf-accuracy: can specify pre/eff filename separately
    rf-accuracy: use tpr and tnr
    rf-accuracy: can specify the precondition/effect directory
    rf-accuracy acc2: compute accuracies from tpr, tnr
    rf-accuracy: use f-measure instead of accuracy
    rf-accuracy: fixed the tpr, tnr, f-measure definition
    rf-accuracy: fix f-measure
    table2: f
    rf-accuracy: for the new log
    rf-accuracy: float
    rf-accuracy: handle cases where the invalid/valid dataset failed to contain examples
    table: use f, not accuracy
    table: also generate bitwise product
    table: both with and without successors
    rf-accuracy: also accuracy
    table: rf-performance: correct header
    
    [script] squashed commits
    
    [script] noprecond
    [script] separated the dump process
    [script] cleanup
    [script] mod
    [script] remove noprecond in favor of TRUE
    [script] lookahead precondition classifier
    [script] update
    [script] refactoring the script
    [script] fix lookahead fake data generation -- affects the output pathnames
    [script] added 18
    [script] always use plurals for actions
    [script] fix for new api in dsama
    [script] fix: action 0 was not enumerated! also, only iterate over the available actions
    [script] precondition now takes more than 1h
    [script] precondition-with-successors
    [script] rf-accuracy-with-successors
    [script] make bagging ratio a variable
    [script] fix
    [script] nobalance
    [script] fix with-successors and nobalance
    [script] to-pddl-with-successors
    [script] run with-successors and nobalance
    [script] create subdirectories
    [script] make it 32g just in case
    [script] predir must end with /
    [script] added ad2 -- uses the current state only
    [script] nobalance do not make sense
    [script] fix for new train-precondition
    [script] tuning cores and queues to run safely
    [script] use alpha=0.5 --- alpha=0.7 sometimes fails to converge
    [script] watch-proj can wait multiple projects
    [script] generate tables
    [script] watch-proj can wait multiple projects
    [script] lazy execution

--- train_actions.sh
+++ train_actions.sh
@@ -6,6 +6,8 @@ trap exit SIGINT
 
 export SHELL=/bin/bash
 ulimit -v 16000000000
+make -C lisp -j 1
+
 
 dir=$(dirname $(dirname $(readlink -ef $0)))
 
@@ -25,8 +27,7 @@ parallel $common PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 {} \
          ::: learn_test_dump \
          ::: direct
 
-watch-proj $proj1
-watch-proj $proj2
+watch-proj $proj1 $proj2 # wait
 
 proj=$(date +%Y%m%d%H%M)ad
 common="jbsub -mem 64g -cores 1+1 -queue x86_1h -proj $proj"
@@ -35,11 +36,14 @@ parallel $common PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 {} \
          ::: samples/* \
          ::: learn_test_dump
 
-
-proj=$(date +%Y%m%d%H%M)
-common="jbsub -mem 16g -cores 1 -queue x86_1h -proj $proj"
-
-make -C lisp -j 1
+proj=$(date +%Y%m%d%H%M)ad2
+common="jbsub -mem 64g -cores 1+1 -queue x86_1h -proj $proj"
+parallel $common PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 {} \
+         ::: ./action_discriminator.py \
+         ::: samples/* \
+         ::: learn_test_dump \
+         ::: prepare_aae_PU4 \
+         ::: _ad2/
 
 actionlearner (){
     lisp/domain-actionlearner.bin $1/actions.csv > $1/actionlearner.pddl 2>$1/actionlearner.err
@@ -47,31 +51,29 @@ actionlearner (){
 
 export -f actionlearner
 
-parallel $common actionlearner ::: samples/*/
+dsama-dump (){
+    dsama dump-tsv $1 ${1%%csv}fasl
+}
 
-
-common="jbsub -mem 16g -cores 8 -queue x86_6h -proj $proj"
+export -f dsama-dump
 
 export ntrees="80 40 20 10 5 2 1"
-export depths="100 50 25 12 7 4"
+export depths="100 50 25 18 12 7 4"
+export bagging="0.66"
 
 dsama-effect (){
     dir=$1
-    [ -e $dir/action+ids.csv ]       || (tr "[:blank:]" "\t" < $dir/actions.csv | paste - $dir/action_ids.csv > $dir/action+ids.csv)
-    [ -e $dir/action+ids.fasl ]      || (dsama dump-tsv $dir/action+ids.csv $dir/action+ids.fasl )
-
     echo $dir
     echo $(readlink -ef $dir)
     cd $dir
     shift
-    actions=$(jq .parameters.M _aae/aux.json)
-    parallel -j 16 --verbose "dsama train-model action+ids.fasl \"(:effect {} $@)\"" \
-             ::: $(seq $actions) \
+    parallel -j 16 --verbose dsama train-effect actions+ids.fasl \
+             ::: $(cat available_actions.csv) \
              ::: binary-random-forest-classifier \
              ::: :n-tree \
              ::: $ntrees \
              ::: :bagging-ratio \
-             ::: 0.34 \
+             ::: ${bagging} \
              ::: :max-depth \
              ::: $depths
     true
@@ -79,28 +81,19 @@ dsama-effect (){
 
 export -f dsama-effect
 
-parallel $common dsama-effect ::: samples/*/
-
-
-common="jbsub -mem 16g -cores 8 -queue x86_6h -proj $proj"
-
 dsama-precondition (){
     dir=$1
-    [ -e $dir/action+ids.csv ]       || (tr "[:blank:]" "\t" < $dir/actions.csv | paste - $dir/action_ids.csv > $dir/action+ids.csv)
-    [ -e $dir/action+ids.fasl ]      || (dsama dump-tsv $dir/action+ids.csv $dir/action+ids.fasl )
-
     echo $dir
     echo $(readlink -ef $dir)
     cd $dir
     shift
-    actions=$(jq .parameters.M _aae/aux.json)
-    parallel -j 16 --verbose "dsama train-model action+ids.fasl \"(:precondition {} $@)\"" \
-             ::: $(seq $actions) \
-             ::: binary-random-forest-classifier pu-random-forest-classifier \
+    parallel -j 16 --verbose "[ -f PRECONDITION/{1}/PU-RANDOM-FOREST-CLASSIFIER-N-TREE-{4}-BAGGING-RATIO-{6}-MAX-DEPTH-{8}.model ] || dsama train-precondition actions+ids.fasl fake_actions+ids.fasl valid_actions+ids.fasl invalid_actions+ids.fasl {}" \
+             ::: $(cat available_actions.csv) \
+             ::: pu-random-forest-classifier \
              ::: :n-tree \
              ::: $ntrees \
              ::: :bagging-ratio \
-             ::: 0.34 \
+             ::: ${bagging} \
              ::: :max-depth \
              ::: $depths
     true
@@ -108,48 +101,46 @@ dsama-precondition (){
 
 export -f dsama-precondition
 
-parallel $common dsama-precondition ::: samples/*/
+dsama-precondition-with-successors (){
+    dir=$1
+    echo $dir
+    echo $(readlink -ef $dir)
+    cd $dir
+    shift
+    parallel -j 16 --verbose "[ -f PRECONDITION-WITH-SUCCESSORS/{1}/PU-RANDOM-FOREST-CLASSIFIER-N-TREE-{4}-BAGGING-RATIO-{6}-MAX-DEPTH-{8}.model ] || dsama train-precondition-with-successors actions+ids.fasl fake_actions+ids.fasl valid_actions+ids.fasl invalid_actions+ids.fasl {}" \
+             ::: $(cat available_actions.csv) \
+             ::: pu-random-forest-classifier \
+             ::: :n-tree \
+             ::: $ntrees \
+             ::: :bagging-ratio \
+             ::: ${bagging} \
+             ::: :max-depth \
+             ::: $depths
+    true
+}
 
-
-
-watch-proj $proj
-
-proj=$(date +%Y%m%d%H%M)accuracy
-common="jbsub -mem 16g -cores 8 -queue x86_1h -proj $proj"
+export -f dsama-precondition-with-successors
 
 rf-accuracy (){
-    ./rf-accuracy.ros --pre $2-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-0.34-MAX-DEPTH-$5.json \
-                      --eff $3-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-0.34-MAX-DEPTH-$5.json \
-                      $1 | tee $1$2-$3-$4-$5.csv
+    mkdir -p $1accuracy
+    ./rf-accuracy.ros --pre $2-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-${bagging}-MAX-DEPTH-$5.json \
+                      --eff $3-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-${bagging}-MAX-DEPTH-$5.json \
+                      $1 | tee $1accuracy/$2-$3-$4-$5.csv
 }
 
 export -f rf-accuracy
 
-parallel $common rf-accuracy  \
-         ::: samples/*/ \
-         ::: PU \
-         ::: BINARY \
-         ::: $ntrees \
-         ::: $depths
-
-proj=$(date +%Y%m%d%H%M)pddl
-common="jbsub -mem 16g -cores 1 -queue x86_1h -proj $proj"
-
-to-pddl (){
-    models=$(mktemp)
-    trap "rm $models" RETURN
-    cd $1
-    # ls -v is necessary because bash will reorder them in a wrong manner e.g. 1, 10, 11, 12, ... 
-    parallel --keep-order "echo {1}/best.model \$(ls -v {2}/*/best.model)" \
-             ::: $(ls -dv PRECONDITION/*) \
-             :::+ $(ls -dv EFFECT/*) \
-             > $models
-    dsama to-pddl $models dsama.pddl
+rf-accuracy-with-successors (){
+    mkdir -p $1accuracy-with-successors
+    ./rf-accuracy.ros --pre $2-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-${bagging}-MAX-DEPTH-$5.json \
+                      --eff $3-RANDOM-FOREST-CLASSIFIER-N-TREE-$4-BAGGING-RATIO-${bagging}-MAX-DEPTH-$5.json \
+                      --predir PRECONDITION-WITH-SUCCESSORS/ \
+                      $1 | tee $1accuracy-with-successors/$2-$3-$4-$5.csv
 }
 
-export -f to-pddl
+export -f rf-accuracy-with-successors
 
-to-pddl-custom (){
+to-pddl (){
     models=$(mktemp)
     trap "rm $models" RETURN
     cd $1
@@ -157,16 +148,61 @@ to-pddl-custom (){
     eff=$3
     ntree=$4
     depth=$5
-    # ls -v is necessary because bash will reorder them in a wrong manner e.g. 1, 10, 11, 12, ... 
-    parallel --keep-order "echo {1}/${pre}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-0.34-MAX-DEPTH-${depth}.model \$(ls -v {2}/*/${eff}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-0.34-MAX-DEPTH-${depth}.model)" \
-             ::: $(ls -dv PRECONDITION/*) \
-             :::+ $(ls -dv EFFECT/*) \
-             > $models
-    dsama to-pddl $models dsama-custom-$pre-$eff-$ntree-$depth.pddl
+    mkdir -p PDDL
+    if ! [ -f PDDL/dsama-$pre-$eff-$ntree-$depth.pddl ]
+    then
+        # ls -v is necessary because bash will reorder them in a wrong manner e.g. 1, 10, 11, 12, ... 
+        parallel --keep-order "echo {1}/${pre}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-${bagging}-MAX-DEPTH-${depth}.model \$(ls -v {2}/*/${eff}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-${bagging}-MAX-DEPTH-${depth}.model)" \
+                 ::: $(ls -dv PRECONDITION/*) \
+                 :::+ $(ls -dv EFFECT/*) \
+                 > $models
+        dsama to-pddl $models PDDL/dsama-$pre-$eff-$ntree-$depth.pddl
+    fi
 }
 
-export -f to-pddl-custom
+export -f to-pddl
 
-# parallel $common ::: to-pddl ::: samples/*/
-parallel $common ::: to-pddl-custom ::: samples/*/ ::: PU ::: BINARY ::: $ntrees ::: $depths
+to-pddl-with-successors (){
+    models=$(mktemp)
+    trap "rm $models" RETURN
+    cd $1
+    pre=$2
+    eff=$3
+    ntree=$4
+    depth=$5
+    mkdir -p PDDL-WITH-SUCCESSORS
+    if ! [ -f PDDL-WITH-SUCCESSORS/dsama-$pre-$eff-$ntree-$depth.pddl ]
+    then
+        # ls -v is necessary because bash will reorder them in a wrong manner e.g. 1, 10, 11, 12, ... 
+        parallel --keep-order "echo {1}/${pre}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-${bagging}-MAX-DEPTH-${depth}.model \$(ls -v {2}/*/${eff}-RANDOM-FOREST-CLASSIFIER-N-TREE-${ntree}-BAGGING-RATIO-${bagging}-MAX-DEPTH-${depth}.model)" \
+                 ::: $(ls -dv PRECONDITION-WITH-SUCCESSORS/*) \
+                 :::+ $(ls -dv EFFECT/*) \
+                 > $models
+        dsama to-pddl $models PDDL-WITH-SUCCESSORS/dsama-$pre-$eff-$ntree-$depth.pddl
+    fi
+}
+
+export -f to-pddl-with-successors
+
+proj2=$(date +%Y%m%d%H%M)pre
+common="jbsub -mem 32g -cores 16 -queue x86_6h -proj $proj2"
+parallel $common dsama-precondition                 ::: samples/*/
+parallel $common dsama-precondition-with-successors ::: samples/*/
+
+watch-proj $proj1 $proj2 # wait
+
+proj1=$(date +%Y%m%d%H%M)accuracy
+common="jbsub -mem 32g -cores 8 -queue x86_1h -proj $proj1"
+parallel $common rf-accuracy                 ::: samples/*/ ::: PU ::: BINARY ::: $ntrees ::: $depths
+parallel $common rf-accuracy-with-successors ::: samples/*/ ::: PU ::: BINARY ::: $ntrees ::: $depths
+
+proj2=$(date +%Y%m%d%H%M)pddl
+common="jbsub -mem 32g -cores 1 -queue x86_1h -proj $proj2"
+parallel $common ::: to-pddl                 ::: samples/*/ ::: PU ::: BINARY ::: $ntrees ::: $depths
+parallel $common ::: to-pddl-with-successors ::: samples/*/ ::: PU ::: BINARY ::: $ntrees ::: $depths
+
+watch-proj $proj1 $proj2
+./table1.sh > samples/table1
+./table2.sh > samples/table2
+./table3.sh > samples/table3
 
--- train_all.sh
+++ train_all.sh
@@ -7,15 +7,15 @@ trap exit SIGINT
 ulimit -v 16000000000
 
 dir=$(dirname $(dirname $(readlink -ef $0)))
-proj=$(date +%Y%m%d%H%M)
+proj=$(date +%Y%m%d%H%M)sae
 # Example command for lsf job scheduler
-common="--dry-run --keep-order -j 1 jbsub -mem 64g -cores 1+1 -queue x86_1h -proj $proj PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1"
+common="--keep-order -j 1 jbsub -mem 64g -cores 1+1 -queue x86_1h -proj $proj PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1"
 
-parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE mandrill 3 3 ::: 100 ::: 10000 ::: 0.0 0.7 ::: 0.0
-parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE mnist 3 3    ::: 100 ::: 10000 ::: 0.0 0.7 ::: 0.0
-parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE spider 3 3   ::: 100 ::: 10000 ::: 0.0 0.7 ::: 0.0
+parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE mandrill 3 3 ::: 100 ::: 10000 ::: 0.0 0.5 ::: 0.0
+parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE mnist 3 3    ::: 100 ::: 10000 ::: 0.0 0.5 ::: 0.0
+parallel $common ./strips.py learn_plot_dump puzzle HammingTransitionAE spider 3 3   ::: 100 ::: 10000 ::: 0.0 0.5 ::: 0.0
 
-parallel $common ./strips.py learn_plot_dump lightsout HammingTransitionAE digital 4 ::: 100 ::: 10000 ::: 0.0 0.7 ::: 0.0
-parallel $common ./strips.py learn_plot_dump lightsout HammingTransitionAE twisted 4 ::: 100 ::: 10000 ::: 0.0 0.7 ::: 0.0
+parallel $common ./strips.py learn_plot_dump lightsout HammingTransitionAE digital 4 ::: 100 ::: 10000 ::: 0.0 0.5 ::: 0.0
+parallel $common ./strips.py learn_plot_dump lightsout HammingTransitionAE twisted 4 ::: 100 ::: 10000 ::: 0.0 0.5 ::: 0.0
 
-# parallel $morecommon ./strips.py learn_plot_dump  hanoi HammingTransitionAE 4 4           ::: 100 1000 ::: 10000 ::: 0.0 0.2 0.5 1.0 ::: 0.0 0.1 0.2 0.5 1.0
+# parallel $morecommon ./strips.py learn_plot_dump  hanoi HammingTransitionAE 4 4           ::: 100 1000 ::: 10000 ::: 0.0 0.5 ::: 0.0
