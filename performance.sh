b10ad6da651789178f0ebbaaaed35baf2d1743bf
Masataro Asai <guicho2.71828@gmail.com>
2020-01-30 16:08:16 -0500

    [ijcai] table generation scripts

--- performance.sh
+++ performance.sh
@@ -3,6 +3,7 @@
 if [ -z "$*" ]
 then
     echo "Usage: $0 [QUERY...]" >&2
+    echo "Usage: $0 [DIRECTORIES...] -- [QUERY...]" >&2
     echo "Example:"             >&2
     echo "$0 .sae.MSE.vanilla.test .sae.inactive.both.test .sae.hamming.test .sae.parameters.zerosuppress .sae.parameters.locality .aae.parameters.lr  .ad.parameters.lr  .sd.parameters.lr " >&2
     exit 1
@@ -11,54 +12,93 @@ fi
 export samples=samples
 export SHELL=/bin/bash
 
-json (){
-    root=${1%%/}
-    filename=$2
-    sae=$(cat ${root}/$filename      2>/dev/null || echo "{}")
-    aae=$(cat ${root}/_aae/$filename 2>/dev/null || echo "{}")
-    sd=$(cat ${root}/_sd3/$filename  2>/dev/null || echo "{}")
-    ad=$(cat ${root}/_ad/$filename   2>/dev/null || echo "{}")
-    ad2=$(cat ${root}/_ad2/$filename   2>/dev/null || echo "{}")
-    echo "{\"sae\": $sae, \"aae\": $aae, \"sd\": $sd, \"ad\": $ad, \"ad2\": $ad2 } "
-}
-
-export -f json
-
-row (){
-    if json $2 performance.json | jq -e $1 &>/dev/null
-    then
-        json $2 performance.json | jq -e $1
-        exit
-    fi
-    if json $2 aux.json | jq -e $1 &>/dev/null
-    then
-        json $2 aux.json | jq -e $1
-        exit
-    fi
-    echo nan
-}
-
-export -f row
-
-col (){
-    f=$(mktemp) 
-    echo $1 > $f
-    parallel --keep-order row $1 :::  $samples/*/ >> $f
-    echo $f
-}
-
-export -f col
-
-header (){
+sep (){
     f=$(mktemp)
-    echo dir > $f
-    ls -vd $samples/*/ >> $f
+    for arg in $@
+    do
+        if [[ "$arg" == "--" ]]
+        then
+            : remaining args are queries
+            echo -n "$f "
+            f=$(mktemp)
+        else
+            echo "$arg" >> $f
+        fi
+    done
     echo $f
 }
 
-export -f header
+sep $@ | {
+    read first second
+    if [[ -z $second ]]
+    then
+        directories="samples/*/"
+        queries="$(cat $first)"
+    else
+        directories=$(cat $first)
+        queries="$(cat $second)"
+    fi
+    export directories queries
 
-files=$(header ; parallel --keep-order col ::: $@)
-paste $files | column -t
-rm $files
 
+    rm $first $second
+
+    json (){
+        local root=${1%%/}
+        local filename=$2
+        local sae=$(cat ${root}/$filename      2>/dev/null || echo "{}")
+        local aae=$(cat ${root}/_aae/$filename 2>/dev/null || echo "{}")
+        local sd=$(cat ${root}/_sd3/$filename  2>/dev/null || echo "{}")
+        local ad=$(cat ${root}/_ad/$filename   2>/dev/null || echo "{}")
+        local ad2=$(cat ${root}/_ad2/$filename   2>/dev/null || echo "{}")
+        echo "{\"sae\": $sae, \"aae\": $aae, \"sd\": $sd, \"ad\": $ad, \"ad2\": $ad2 } "
+    }
+
+    export -f json
+
+    row (){
+        performance=$(json "$2" performance.json)
+        aux=$(json "$2" aux.json)
+        if (echo "$performance $aux" | jq -s ".[0] * .[1]" | jq -e "$1" &>/dev/null)
+        then
+            (echo "$performance $aux" | jq -s ".[0] * .[1]" | jq -e "$1")
+        else
+            echo nan
+        fi
+    }
+
+    export -f row
+
+    col (){
+        local f=$(mktemp) 
+        echo $1 > $f
+        # for d in $directories
+        # do
+        #     row "$1" $d >> $f
+        # done
+        # parallel --keep-order "row '$1'" ::: $directories | ./colorize.ros -i >> $f
+        parallel --keep-order "row '$1'" ::: $directories >> $f
+        echo $f
+    }
+
+    export -f col
+
+    header (){
+        local f=$(mktemp)
+        echo directory > $f
+        # for dir in $directories
+        # do
+        #     echo $dir >> $f
+        # done
+        parallel --keep-order echo ::: $directories >> $f
+        echo $f
+    }
+
+    export -f header
+
+    # files=$(header ; for query in $queries ; do col "$query" ; done)
+    files=$(header ; parallel --keep-order col ::: $queries)
+    paste $files | column -t
+    rm $files
+
+}
