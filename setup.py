de8da8283b2d07bac835ec2bfff73b62772ad9dc
Masataro Asai <guicho2.71828@gmail.com>
2020-06-29 01:34:24 -0400

    setuptool setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='latplan',
+      version='0.0.1',
+      install_requires=[
+          'tensorflow==1.15',
+          'keras==2.2.5',
+          'h5py',
+          'matplotlib',
+          'progressbar2',
+          'keras-adabound',
+          'keras-rectified-adam',
+          'timeout_decorator',
+          'ansicolors',
+          'scipy==1.4.1',
+          'scikit-image',
+          'imageio',
+          'pillow',],
+      packages=find_packages(),
+      include_package_data=True,
+)
