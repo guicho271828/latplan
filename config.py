cb9ef5905bcb654020de0359e866c5eb2648e453
Masataro Asai <guicho2.71828@gmail.com>
2020-08-05 11:27:10 -0400

    use tf.compat.v1 to suppress warning

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ print("Default float: {}".format(K.floatx()))
 
 def load_session():
     K.set_session(
-        tf.Session(
-            config=tf.ConfigProto(
+        tf.compat.v1.Session(
+            config=tf.compat.v1.ConfigProto(
                 allow_soft_placement=True,
                 intra_op_parallelism_threads=1,
                 inter_op_parallelism_threads=1,
                 device_count = {'CPU': 1, 'GPU': 1},
                 gpu_options =
-                tf.GPUOptions(
+                tf.compat.v1.GPUOptions(
                     per_process_gpu_memory_fraction=1.0,
                     allow_growth=True,))))
 
