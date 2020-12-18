233db9846fb98a46783d316c32b6e5d32d0f63a7
Masataro Asai <guicho2.71828@gmail.com>
2020-12-18 15:05:20 -0500

    backport: refactor setup-dataset.sh

--- setup-dataset.sh
+++ setup-dataset.sh
@@ -9,20 +9,25 @@ trap exit SIGINT
 ulimit -v 16000000000
 
 proj=$(date +%Y%m%d%H%M)setup
-common="jbsub -mem 64g -cores 1 -queue x86_1h -proj $proj"
+
+# without job scheduler
+common="PYTHONUNBUFFERED=1"
+
+# with job scheduler
+common="jbsub -mem 64g -cores 1 -queue x86_6h -proj $proj PYTHONUNBUFFERED=1"
 
 # some instances clearly does not have 50000 states/transitiosn, but lets forget about it for now
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mnist 3 3 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mnist 4 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mandrill 3 3 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle mandrill 4 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle spider 3 3 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py puzzle spider 4 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout digital 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout digital 5 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout twisted 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py lightsout twisted 5 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 3 3 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 4 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 9 3 50000
-$common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 8 50000
+$common ./setup-dataset.py puzzle mnist 3 3 50000
+$common ./setup-dataset.py puzzle mnist 4 4 50000
+$common ./setup-dataset.py puzzle mandrill 3 3 50000
+$common ./setup-dataset.py puzzle mandrill 4 4 50000
+$common ./setup-dataset.py puzzle spider 3 3 50000
+$common ./setup-dataset.py puzzle spider 4 4 50000
+$common ./setup-dataset.py lightsout digital 4 50000
+$common ./setup-dataset.py lightsout digital 5 50000
+$common ./setup-dataset.py lightsout twisted 4 50000
+$common ./setup-dataset.py lightsout twisted 5 50000
+$common ./setup-dataset.py hanoi 3 3 50000
+$common ./setup-dataset.py hanoi 4 4 50000
+$common ./setup-dataset.py hanoi 9 3 50000
+$common ./setup-dataset.py hanoi 4 8 50000
