fff3626937d7a420ef6a3c84922d186be88ffd82
Masataro Asai <guicho2.71828@gmail.com>
2020-12-18 15:07:23 -0500

    backport: download blocksworld dataset

--- setup-dataset.sh
+++ setup-dataset.sh
@@ -31,3 +31,21 @@ $common ./setup-dataset.py hanoi 3 3 50000
 $common ./setup-dataset.py hanoi 4 4 50000
 $common ./setup-dataset.py hanoi 9 3 50000
 $common ./setup-dataset.py hanoi 4 8 50000
+
+download-and-extract (){
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1.npz -O latplan/puzzles/$1.npz
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1-init.json -O latplan/puzzles/$1-init.json
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1-stat.json -O latplan/puzzles/$1-stat.json
+}
+
+export -f download-and-extract
+
+$common download-and-extract blocks-5-3
+$common download-and-extract blocks-4-4
+$common download-and-extract blocks-3-7
+$common download-and-extract blocks-3-6
+$common download-and-extract blocks-3-5
+$common download-and-extract blocks-3-4
+$common download-and-extract blocks-3-3
+$common download-and-extract blocks-3-3-multi
+$common download-and-extract blocks-3-3-multi-global
