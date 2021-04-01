3680e85f61db8479bd323c1f6cf7fa3663e75f30
Masataro Asai <guicho2.71828@gmail.com>
2021-04-01 16:47:31 -0400

    pip setup: dependency is resolved by conda

--- setup.py
+++ setup.py
@@ -4,21 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='latplan',
       version='0.0.1',
-      install_requires=[
-          'tensorflow==1.15.2',
-          'keras==2.2.5',
-          'h5py',
-          'matplotlib',
-          'progressbar2',
-          'keras-adabound',
-          'keras-rectified-adam',
-          'timeout_decorator',
-          'ansicolors',
-          'scipy==1.4.1',
-          'scikit-image',
-          'imageio',
-          'pillow',
-          'pddlgym'],
+      install_requires=[],
       packages=find_packages(),
       include_package_data=True,
 )
