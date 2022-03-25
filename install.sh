59fd8dec4c697dacc5210bb9219fe2da5fa574c6
Masataro Asai <guicho2.71828@gmail.com>
2022-03-25 16:37:22 -0400

    migrating to the new installation script

--- install.sh
+++ install.sh
@@ -1,27 +1,76 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-git submodule update --init --recursive
+env=latplan
 
+# execute it in a subshell so that set -e stops on error, but does not exit the parent shell.
 (
-    cd downward
-    ./build.py -j $(cat /proc/cpuinfo | grep -c processor) release
-)
+    set -e
 
+    (conda activate >/dev/null 2>/dev/null)  || {
+        echo "This script must be sourced, not executed. Run it like: source $0"
+        exit 1
+    }
 
-which ros || (
-    git clone -b release https://github.com/roswell/roswell.git
-    cd roswell
-    sh bootstrap
-    ./configure
-    make
-    sudo make install
-    ros setup
-)
+    conda env create -n $env -f environment.yml || {
+        echo "installation failed; cleaning up"
+        conda env remove -n $env
+        exit 1
+    }
 
-ros dynamic-space-size=8000 install numcl arrival eazy-gnuplot magicffi dataloader
+    conda activate $env
 
-make -j 1 -C lisp
+    git submodule update --init --recursive
 
-pip install -e .
+    ################################################################
+    # reset my local variables so that they do not affect reproducibility
 
-./download-dataset.sh
+    echo "path variables that are set locally:"
+    echo "-----------------"
+    env | grep PATH | cut -d= -f1
+    echo "-----------------"
+    echo
+
+    conda env config vars set LD_LIBRARY_PATH=
+    conda env config vars set PYTHONPATH=
+    conda env config vars set PKG_CONFIG_PATH=
+
+    # these are required for building dependencies properly under conda
+    conda env config vars set CPATH=${CONDA_PREFIX}/include:${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/include:
+    conda env config vars set ROSWELL_HOME=$(readlink -ef .roswell)
+
+    # note: "variables" field in yaml file introduced in conda 4.9 does not work because it does not expand shell variables
+
+    # these are required for the variables to take effect
+    conda deactivate ; conda activate $env
+
+    echo $CPATH ; echo $LD_LIBRARY_PATH
+
+    conda env config vars list
+
+    ################################################################
+    # build fast downward
+
+    (
+        cd downward
+        ./build.py -j $(cat /proc/cpuinfo | grep -c processor) release
+    )
+
+    ################################################################
+    # build ARRIVAL validator through roswell
+    (
+        cd roswell
+        sh bootstrap
+        ./configure --prefix=${CONDA_PREFIX}
+        make
+        make install
+        ros setup
+        ros dynamic-space-size=8000 install numcl arrival eazy-gnuplot magicffi dataloader
+    )
+
+    make -j 1 -C lisp
+    ./download-dataset.sh
+
+) && {
+    conda activate $env >/dev/null 2>/dev/null || echo "This script must be sourced, not executed. Run it like: source $0"
+}
+
